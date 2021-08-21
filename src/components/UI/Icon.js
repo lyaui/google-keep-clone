c904c1f95557a737a2740076f0fddb66110537a3
Iris Huang <lyaui@outloook.com>
2021-08-22 05:30:38 +0800

    add icons component

---
+++ src/components/UI/Icon.js
@@ -0,0 +1,70 @@
+// menu
+import { MdMenu as Menu } from 'react-icons/md';
+
+// bulb
+import { MdLightbulbOutline as Bulb } from 'react-icons/md';
+
+// checkbox
+import { IoIosCheckbox as Checkbox } from 'react-icons/io';
+import { IoIosCheckboxOutline as CheckboxOutline } from 'react-icons/io';
+
+// edit
+import { HiPencil as Edit } from 'react-icons/hi';
+import { HiOutlinePencil as EditOutline } from 'react-icons/hi';
+
+// label
+import { MdLabel as Label } from 'react-icons/md';
+import { MdLabelOutline as LabelOutline } from 'react-icons/md';
+
+// archive
+import { RiInboxArchiveLine as Archive } from 'react-icons/ri';
+
+// delete
+import { MdDelete as Delete } from 'react-icons/md';
+import { RiDeleteBin6Line as DeleteOutline } from 'react-icons/ri';
+
+// pin
+import { RiPushpin2Fill as Pin } from 'react-icons/ri';
+import { RiPushpin2Line as PinOutline } from 'react-icons/ri';
+
+// add
+import { MdAdd as Add } from 'react-icons/md';
+
+// select
+import { MdCheckCircle as SelectCircle } from 'react-icons/md';
+
+// more
+import { MdMoreVert as More } from 'react-icons/md';
+
+// palette
+import { BiPalette as Palette } from 'react-icons/bi';
+
+// image
+import { BiImage as Image } from 'react-icons/bi';
+
+// view by
+import { IoGridOutline as Grid } from 'react-icons/io5';
+import { BsViewStacked as Stacked } from 'react-icons/bs';
+
+export {
+  Menu,
+  Bulb,
+  Checkbox,
+  CheckboxOutline,
+  Edit,
+  EditOutline,
+  Label,
+  LabelOutline,
+  Archive,
+  Delete,
+  DeleteOutline,
+  Pin,
+  PinOutline,
+  Add,
+  SelectCircle,
+  More,
+  Palette,
+  Image,
+  Grid,
+  Stacked,
+};
