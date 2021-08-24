ae0a2ff8e707881a8695eec837d4a0eb20cb6e54
Iris Huang <lyaui@outloook.com>
2021-08-24 09:24:30 +0800

    import card relative components

--- src/components/UI/Card/index.js
+++ src/components/UI/Card/index.js
@@ -1,7 +1,16 @@
 import { SCard } from 'components/UI/Card/style.js';
+import CardHeader from 'components/UI/Card/CardHeader';
+import CardContent from 'components/UI/Card/CardContent';
+import CardFooter from 'components/UI/Card/CardFooter';
 
-const Crad = ({ color }) => {
-  return <SCard color={color}>card</SCard>;
+const Crad = ({ color, title, content }) => {
+  return (
+    <SCard color={color}>
+      <CardHeader>{title}</CardHeader>
+      <CardContent>{content}</CardContent>
+      <CardFooter />
+    </SCard>
+  );
 };
 
 export default Crad;
