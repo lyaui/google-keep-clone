3a1e270507ea3716413d5b7b3df524549fea8da6
Iris Huang <lyaui@outloook.com>
2021-08-29 21:28:19 +0800

    add masonry card style

--- src/components/UI/Card/index.js
+++ src/components/UI/Card/index.js
@@ -1,14 +1,48 @@
+import { useState, useEffect, useRef } from 'react';
 import { SCard } from 'components/UI/Card/style.js';
 import CardHeader from 'components/UI/Card/CardHeader';
 import CardContent from 'components/UI/Card/CardContent';
 import CardFooter from 'components/UI/Card/CardFooter';
 
-const Crad = ({ color, title, content }) => {
+const Crad = ({ color, title, content, masonryDom }) => {
+  const cardRef = useRef();
+  const [gridRowSpan, setGridRowSpan] = useState(0);
+
+  const getRowSpan = () => {
+    const cardRefDOM = cardRef.current;
+    const grid = masonryDom;
+    if (!grid || !cardRefDOM) return;
+
+    const rowGap = parseInt(
+      window.getComputedStyle(grid).getPropertyValue('grid-row-gap').replace('px', ''),
+    );
+    const rowHeight = parseInt(
+      window.getComputedStyle(grid).getPropertyValue('grid-auto-rows').replace('px', ''),
+    );
+
+    const rowSpan = Math.ceil(
+      (cardRefDOM.querySelector('.growing-content').getBoundingClientRect().height + rowGap) /
+        (rowHeight + rowGap),
+    );
+    setGridRowSpan(rowSpan);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', getRowSpan);
+    getRowSpan();
+
+    return () => {
+      window.removeEventListener('resize', getRowSpan);
+    };
+  });
+
   return (
-    <SCard color={color}>
-      <CardHeader>{title}</CardHeader>
-      <CardContent>{content}</CardContent>
-      <CardFooter />
+    <SCard color={color} className='card' ref={cardRef} gridRowSpan={gridRowSpan}>
+      <div className='growing-content'>
+        <CardHeader>{title}</CardHeader>
+        <CardContent>{content}</CardContent>
+        <CardFooter />
+      </div>
     </SCard>
   );
 };
--- src/components/UI/Card/style.js
+++ src/components/UI/Card/style.js
@@ -2,11 +2,10 @@ import styled from 'styled-components/macro';
 
 export const SCard = styled.div`
   background-color: ${(props) => props.color || '#FFFFFF'};
-  flex-basis: calc(25% - 16px);
-  margin: 8px;
   border: 1px solid ${(props) => props.color || '#e0e0e0'};
   border-radius: 8px;
   cursor: pointer;
+  grid-row: ${(props) => `span ${props.gridRowSpan}`};
   transition: all 0.3s;
   :hover {
     box-shadow: 0px 0px 3px 1px rgba(0, 0, 0, 0.2);
