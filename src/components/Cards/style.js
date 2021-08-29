628a212f3c1152b51aa9716067b2dad70e3b3ce5
Iris Huang <lyaui@outloook.com>
2021-08-29 21:26:46 +0800

    replace flex with grid

--- src/components/Cards/style.js
+++ src/components/Cards/style.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components/macro';
 
 export const SCards = styled.section`
-  display: flex;
-  flex-wrap: wrap;
-  width: 100%;
-  padding: 20px;
+  display: grid;
+  grid-gap: 8px;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-auto-rows: 0;
+  padding: 20px 60px;
 `;
