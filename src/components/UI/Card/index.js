041eb6843805cdc7e0d4c9c37e93da956188f933
Iris Huang <lyaui@outloook.com>
2021-08-31 22:57:03 +0800

    add Draggable component

--- src/components/UI/Card/index.js
+++ src/components/UI/Card/index.js
@@ -1,49 +1,68 @@
 import { useState, useEffect, useRef } from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+import composeRefs from '@seznam/compose-react-refs';
 import { SCard } from 'components/UI/Card/style.js';
 import CardHeader from 'components/UI/Card/CardHeader';
 import CardContent from 'components/UI/Card/CardContent';
 import CardFooter from 'components/UI/Card/CardFooter';
 
-const Crad = ({ color, title, content, masonryDom }) => {
+const Crad = ({ id, index, color, title, content, masonryDom }) => {
   const cardRef = useRef();
   const [gridRowSpan, setGridRowSpan] = useState(0);
 
+  // 計算每張卡片跨列 span
   const getRowSpan = () => {
     const cardRefDOM = cardRef.current;
     const grid = masonryDom;
     if (!grid || !cardRefDOM) return;
 
     const rowGap = parseInt(
-      window.getComputedStyle(grid).getPropertyValue('grid-row-gap').replace('px', ''),
+      window
+        .getComputedStyle(grid.querySelector('.masonry'))
+        .getPropertyValue('grid-row-gap')
+        .replace('px', ''),
     );
     const rowHeight = parseInt(
-      window.getComputedStyle(grid).getPropertyValue('grid-auto-rows').replace('px', ''),
+      window
+        .getComputedStyle(grid.querySelector('.masonry'))
+        .getPropertyValue('grid-auto-rows')
+        .replace('px', ''),
     );
-
     const rowSpan = Math.ceil(
       (cardRefDOM.querySelector('.growing-content').getBoundingClientRect().height + rowGap) /
         (rowHeight + rowGap),
     );
+
     setGridRowSpan(rowSpan);
   };
 
   useEffect(() => {
     window.addEventListener('resize', getRowSpan);
     getRowSpan();
-
     return () => {
       window.removeEventListener('resize', getRowSpan);
     };
   });
 
   return (
-    <SCard color={color} className='card' ref={cardRef} gridRowSpan={gridRowSpan}>
-      <div className='growing-content'>
-        <CardHeader>{title}</CardHeader>
-        <CardContent>{content}</CardContent>
-        <CardFooter />
-      </div>
-    </SCard>
+    <Draggable draggableId={id} index={index}>
+      {(provided) => (
+        <SCard
+          color={color}
+          className='card'
+          ref={composeRefs(cardRef, provided.innerRef)}
+          gridRowSpan={gridRowSpan}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <div className='growing-content'>
+            <CardHeader>{title}</CardHeader>
+            <CardContent>{content}</CardContent>
+            <CardFooter />
+          </div>
+        </SCard>
+      )}
+    </Draggable>
   );
 };
 
