531a67e902cdcc05767a8a99bd23c904e989fd87
Iris Huang <lyaui@outloook.com>
2021-08-31 22:55:54 +0800

    add DragDropContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 // pages
 import Main from 'pages/Main.js';
@@ -8,21 +9,24 @@ import Header from 'components/layout/Header';
 import SideMenu from 'components/layout/SideMenu';
 
 function App() {
+  const onDragEnd = (result) => {};
   return (
-    <BrowserRouter>
-      <Header />
-      <div className='notes-container'>
-        <SideMenu />
-        <main>
-          <Switch>
-            <Route path='/:label'>
-              <Main />
-            </Route>
-            <Redirect to='/home' />
-          </Switch>
-        </main>
-      </div>
-    </BrowserRouter>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <BrowserRouter>
+        <Header />
+        <div className='notes-container'>
+          <SideMenu />
+          <main>
+            <Switch>
+              <Route path='/:label'>
+                <Main />
+              </Route>
+              <Redirect to='/home' />
+            </Switch>
+          </main>
+        </div>
+      </BrowserRouter>
+    </DragDropContext>
   );
 }
 
