921662733e6d8c4602b7adfad9beb047fe0b44ea
Iris Huang <lyaui@outloook.com>
2021-09-16 20:03:54 +0800

    change card width

--- src/components/Cards/style.js
+++ src/components/Cards/style.js
@@ -3,7 +3,7 @@ import styled from 'styled-components/macro';
 export const SCards = styled.section`
   display: grid;
   grid-gap: 8px;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   grid-auto-rows: 0;
   padding: 20px 60px;
 `;
