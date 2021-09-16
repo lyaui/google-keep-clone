53d67b06cd546577e3c87c536eca5157c671d62d
Iris Huang <lyaui@outloook.com>
2021-09-16 14:39:22 +0800

    fix numOfMoreLabel value

--- src/components/UI/Card/CardLabels/index.js
+++ src/components/UI/Card/CardLabels/index.js
@@ -3,7 +3,7 @@ import { SCardLabels } from 'components/UI/Card/CardLabels/style.js';
 
 const CardLabels = ({ labels }) => {
   const showLabelsNum = 3;
-  const numOfMoreLabel = labels.length - showLabelsNum;
+  const numOfMoreLabel = labels.length - showLabelsNum - 1;
   return (
     <SCardLabels>
       {labels.slice(0, 3).map((label) => (
