268abc3295c47fe78bebe68728d5c6c5b1aa49d9
Iris Huang <lyaui@outloook.com>
2021-09-17 18:04:22 +0800

    add default colors constant

---
+++ src/constants/paletteColors.js
@@ -0,0 +1,14 @@
+export const PALETTE_COLORS = {
+  DEAFULT: '#e0e0e0',
+  RED: '#f28b82',
+  ORANGE: '#fbbc04',
+  YELLOW: '#fff475',
+  GREEN: '#ccff90',
+  TEAL: '#a7ffeb',
+  BLUE: '#cbf0f8',
+  DARK_BLUE: '#aecbfa',
+  PURPLE: '#d7aefb',
+  PINK: '#fdcfe8',
+  BROWN: '#e6c9a8',
+  GRAY: '#e8eaed',
+};
