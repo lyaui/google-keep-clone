943aa44cac467c7395ad82f677e7fbfb87ab3801
Iris Huang <lyaui@outloook.com>
2021-09-17 17:21:15 +0800

    add square button

--- src/components/UI/Buttons/index.js
+++ src/components/UI/Buttons/index.js
@@ -1,9 +1,17 @@
-import { SButtonRound } from 'components/UI/Buttons/style.js';
+import { SButtonRound, SButtonSquare } from 'components/UI/Buttons/style.js';
 
 export const ButtonRound = ({ children, size, onClick }) => {
   return (
     <SButtonRound size={size} onClick={onClick}>
       {children}
     </SButtonRound>
+  );
+};
+
+export const ButtonSquare = ({ children, size, onClick }) => {
+  return (
+    <SButtonSquare size={size} onClick={onClick}>
+      {children}
+    </SButtonSquare>
   );
 };
--- src/components/UI/Buttons/style.js
+++ src/components/UI/Buttons/style.js
@@ -1,18 +1,32 @@
 import styled from 'styled-components/macro';
 
-export const SButtonRound = styled.button`
+export const SButton = styled.button`
   width: ${(props) => `${props.size}px` || '40px'};
   height: ${(props) => `${props.size}px` || '40px'};
   margin: 4px;
-  border-radius: 50%;
   background-color: transparent;
   transition: all 0.3s;
   svg {
-    width: 80%;
-    height: 80%;
-    fill: #5f6368;
+    width: 90%;
+    height: 90%;
+  }
+`;
+
+export const SButtonRound = styled(SButton)`
+  border-radius: 50%;
+  svg {
+    fill: #202124;
+    opacity: 0.6;
   }
   :hover {
     background-color: rgba(95, 99, 104, 0.157);
+  }
+`;
+
+export const SButtonSquare = styled(SButton)`
+  border-radius: 5px;
+  background-color: rgba(0, 0, 0, 0.7);
+  svg {
+    fill: #fff;
   }
 `;
