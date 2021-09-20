8f77e7dbf619e4114ce4c409a9c976bcc8bde3b3
Iris Huang <lyaui@outloook.com>
2021-09-21 03:22:10 +0800

    add creator property into label schema

--- server/models/Label-model.js
+++ server/models/Label-model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
 const labelSchema = new Schema({
+  creator: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   name: { type: String, required: true, unique: true },
   memos: [{ type: Schema.Types.ObjectId, ref: 'MEMO', default: [] }],
 });
