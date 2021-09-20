e8194f7131638678d6b5c985cac8aa1ce258476c
Iris Huang <lyaui@outloook.com>
2021-09-20 23:26:16 +0800

    add Label model details

--- server/models/Label-model.js
+++ server/models/Label-model.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
-const labelSchema = new Schema({});
+const labelSchema = new Schema({
+  name: { type: String, required: true, unique: true },
+  memos: [{ type: Schema.Types.ObjectId, ref: 'MEMO', default: [] }],
+});
 const Label = model('Label', labelSchema);
 
 module.exports = Label;
