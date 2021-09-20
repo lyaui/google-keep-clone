6790598bebb099094d7096ee21579e39364fbc30
Iris Huang <lyaui@outloook.com>
2021-09-20 23:09:33 +0800

    add User model details

--- server/models/User-model.js
+++ server/models/User-model.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+  name: { type: String, require: true },
+  email: { type: String, require: true, unique: true },
+  password: { type: String, require: true, minlength: 6 },
+  memos: [{ type: Schema.Types.ObjectId, ref: 'Memo', default: [] }],
+  labels: [{ type: Schema.Types.ObjectId, ref: 'Label', default: [] }],
+  settings: {
+    THEME: { type: String, required: true, enum: ['LIGHT', 'DARK'], default: 'LIGHT' },
+    LAYOUT: { type: String, required: true, enum: ['GRID', 'LIST'], default: 'GRID' },
+    SORT: { type: String, required: true, enum: ['ASCEND', 'DESCEND'], default: 'ASCEND' },
+  },
+});
 const User = model('User', userSchema);
 
 module.exports = User;
