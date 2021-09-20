6220761c5e3875dcae3e96f037c807c721d0c40c
Iris Huang <lyaui@outloook.com>
2021-09-21 03:13:35 +0800

    add Memo details

--- server/models/Memo-model.js
+++ server/models/Memo-model.js
@@ -1,7 +1,43 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
-const memoSchema = new Schema({});
+const memoSchema = new Schema(
+  {
+    title: { type: String, default: '' },
+    content: { type: String, default: '' },
+    images: [{ type: String, default: [] }],
+    isPinned: { type: Boolean, defaule: false },
+    isAchived: { type: Boolean, defaule: false },
+    links: [{ type: String, default: [] }],
+    labels: [{ type: Schema.Types.ObjectId, ref: 'Label', default: [] }],
+    tasks: [
+      {
+        task: { type: String, default: '' },
+        isCompleted: { type: Boolean, default: false },
+        default: [],
+      },
+    ],
+    color: {
+      type: String,
+      enum: [
+        'DEFAULT',
+        'RED',
+        'ORANGE',
+        'YELLOW',
+        'GREEN',
+        'TEAL',
+        'BLUE',
+        'DARK_BLUE',
+        'PURPLE',
+        'PINK',
+        'BROWN',
+        'GRAY',
+      ],
+      default: 'DEFAULT',
+    },
+  },
+  { timestamps: true },
+);
 const Memo = model('Memo', memoSchema);
 
 module.exports = Memo;
