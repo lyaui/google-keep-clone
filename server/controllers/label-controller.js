29366b89e025c132a0195f70fac2978679879b48
Iris Huang <lyaui@outloook.com>
2021-09-22 16:31:21 +0800

    add update label api

--- server/controllers/label-controller.js
+++ server/controllers/label-controller.js
@@ -11,7 +11,7 @@ const createLabel = async (req, res, next) => {
 
     // check if label name exists
     const label = await Label.findOne({ $and: [{ creator }, { name: name.trim() }] });
-    if (label) next(new HttpError('Label name has existed, please try another one.', 422));
+    if (label) return next(new HttpError('Label name has existed, please try new one.', 422));
 
     // create label add to user labels
     const session = await mongoose.startSession();
@@ -27,12 +27,39 @@ const createLabel = async (req, res, next) => {
       message: 'Create new label successfully.',
     });
   } catch (err) {
-    console.log(err);
     return next(new HttpError(err));
   }
 };
 
-const updateLabel = async (req, res, next) => {};
+const updateLabel = async (req, res, next) => {
+  const { name, creator } = req.body;
+  const { labelId } = req.params;
+
+  try {
+    // check if user exists
+    const user = await User.findById(creator);
+    if (!user) return next(new HttpError('Could not find user for provided id', 404));
+
+    // check if label exists
+    const label = await Label.findOne({ $and: [{ creator }, { _id: labelId }] });
+    if (!label) return next(new HttpError('Could not find label for provided id.', 404));
+
+    // check if updated label name exists, and update label name
+    label.name = name.trim();
+    const hasSameLabel = await Label.findOne({ $and: [{ creator }, { name: name.trim() }] });
+    if (hasSameLabel)
+      return next(new HttpError('Label name has existed, please try new one.', 422));
+    await label.save();
+
+    res.status(201).json({
+      success: true,
+      data: label,
+      message: 'Update label successfully.',
+    });
+  } catch (err) {
+    return next(new HttpError(err));
+  }
+};
 
 const deleteLabel = async (req, res, next) => {};
 
--- server/routes/labels-route.js
+++ server/routes/labels-route.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const { labelController } = require('../controllers');
 
 router.post('/', labelController.createLabel);
+router.patch('/:labelId', labelController.updateLabel);
 
 module.exports = router;
