4622a7f8098ea95bdc88b959baf4e88b79dbef8e
Iris Huang <lyaui@outloook.com>
2021-09-23 10:53:50 +0800

    fix createMemo api

--- server/controllers/memo-controller.js
+++ server/controllers/memo-controller.js
@@ -1,9 +1,17 @@
-const { HttpError, Memo } = require('../models');
+const mongoose = require('mongoose');
+const { HttpError, Memo, Label, User } = require('../models');
 
 const createMemo = async (req, res, next) => {
-  const { title, content, images, isPinned, isAchived, links, labels, tasks, color } = req.body;
+  const { creator, title, content, images, isPinned, isAchived, links, labels, tasks, color } =
+    req.body;
+
   try {
-    const createdMemo = await new Memo({
+    // check if user exists
+    const user = await User.findById(creator);
+    if (!user) return next(new HttpError('Could not find user for provided id', 404));
+
+    const createdMemo = new Memo({
+      creator,
       title,
       content,
       images,
@@ -13,11 +21,26 @@ const createMemo = async (req, res, next) => {
       labels,
       tasks,
       color,
-    }).save();
+    });
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    // save created memo
+    await createdMemo.save(createdMemo);
+    // add memo to labels
+    await Label.updateMany({ _id: labels }, { $push: { memos: createdMemo._id } });
+    // add memo to user
+    user.memos.push(createdMemo);
+    await user.save({ session });
+    await session.commitTransaction();
+
+    // return data with label details
+    const returnedData = await createdMemo.populate('labels', ['_id', 'name']);
+
     res.status(201).json({
       success: true,
-      data: createdMemo,
-      message: 'create new memo successfully.',
+      data: returnedData,
+      message: 'Create new memo successfully.',
     });
   } catch (err) {
     next(new HttpError(err));
