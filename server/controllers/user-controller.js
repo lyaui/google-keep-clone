e4ee3b568615a24d724c416ab4c752c5b51863d1
Iris Huang <lyaui@outloook.com>
2021-09-24 02:08:18 +0800

    hash and compare user password

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const { HttpError, User } = require('../models');
 
 // TODO 等加入 user auth 直接確認 cretor 是否等於 req.user 並移除 check if user exists
@@ -8,10 +9,25 @@ const signup = async (req, res, next) => {
     const user = await User.findOne({ email });
     if (user) return next(new HttpError('Email has already been registered.', 422));
 
-    const createdUser = await new User({ name: name.trim(), email: email.trim(), password }).save();
+    // hash the password
+    const saultRound = 12;
+    const hashedPassword = await bcrypt.hash(password, saultRound);
+
+    const createdUser = await new User({
+      name: name.trim(),
+      email: email.trim(),
+      password: hashedPassword,
+    }).save();
+
     res.status(201).json({
       success: true,
-      data: createdUser,
+      data: {
+        _id: createdUser._id,
+        name: createdUser.name,
+        email: createdUser.email,
+        settings: createdUser.settings,
+        labels: createdUser.labels,
+      },
       message: 'Signed up successfully.',
     });
   } catch (err) {
@@ -27,10 +43,20 @@ const login = async (req, res, next) => {
     if (!user) return next(new HttpError('User not found.', 404));
 
     // check is correct password
-    if (user.password !== password)
-      return next(new HttpError('Invalid credential, please try again.', 401));
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) return next(new HttpError('Invalid credential, please try again.', 401));
 
-    res.status(200).json({ success: true, message: 'Logined in!' });
+    res.status(200).json({
+      success: true,
+      data: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        settings: user.settings,
+        labels: user.labels,
+      },
+      message: 'Logined in!',
+    });
   } catch (err) {
     return next(new HttpError(err));
   }
