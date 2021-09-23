909c0b4393cf4b8ba86847df7d65f1891480e59c
Iris Huang <lyaui@outloook.com>
2021-09-23 11:54:29 +0800

    add updateMemo api

--- server/controllers/memo-controller.js
+++ server/controllers/memo-controller.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { HttpError, Memo, Label, User } = require('../models');
 
+// TODO 所有的 label 等加入 user auth 直接確認 cretor 是否等於 req.user 並移除 check if user exists
 const createMemo = async (req, res, next) => {
   const { creator, title, content, images, isPinned, isAchived, links, labels, tasks, color } =
     req.body;
@@ -47,4 +48,44 @@ const createMemo = async (req, res, next) => {
   }
 };
 
-module.exports = { createMemo };
+const updateMemo = async (req, res, next) => {
+  const { creator, title, content, images, isPinned, isAchived, links, labels, tasks, color } =
+    req.body;
+  const { memoId } = req.params;
+
+  try {
+    // check if user exists
+    const user = await User.findById(creator);
+    if (!user) return next(new HttpError('Could not find user for provided id', 404));
+
+    // check if memo exists
+    const memo = await Memo.findOne({ $and: [{ creator }, { _id: memoId }] });
+    if (!memo) return next(new HttpError('Could not find memo for provided id.', 404));
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    // update memo
+    const updatedMemo = await Memo.findOneAndUpdate(
+      { $and: [{ creator }, { _id: memoId }] },
+      { title, content, images, isPinned, isAchived, links, labels, tasks, color },
+      { new: true },
+    );
+    // update labels's memo
+    await Label.updateMany({ memos: memoId }, { $pull: { memos: memoId } });
+    await Label.updateMany({ _id: labels }, { $push: { memos: memoId } });
+    await session.commitTransaction();
+
+    // return data with label details
+    const returnedData = await updatedMemo.populate('labels', ['_id', 'name']);
+
+    res.status(200).json({
+      success: true,
+      data: returnedData,
+      message: 'Update label successfully.',
+    });
+  } catch (err) {
+    next(new HttpError(err));
+  }
+};
+
+module.exports = { createMemo, updateMemo };
--- server/routes/memos-route.js
+++ server/routes/memos-route.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const { memoController } = require('../controllers');
 
 router.post('/', memoController.createMemo);
+router.patch('/:memoId', memoController.updateMemo);
 
 module.exports = router;
