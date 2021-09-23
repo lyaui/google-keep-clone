c0e2c3f78d2288d2ba70ad8d91d8eec1c1fe7306
Iris Huang <lyaui@outloook.com>
2021-09-23 18:42:09 +0800

    add getMemosByLabelId  api

--- server/routes/memos-route.js
+++ server/routes/memos-route.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { memoController } = require('../controllers');
 
 router.get('/', memoController.getMemos);
+router.get('/label/:labelId', memoController.getMemosByLabelId);
 router.post('/', memoController.createMemo);
 router.patch('/:memoId', memoController.updateMemo);
 router.delete('/:memoId', memoController.deleteMemo);
