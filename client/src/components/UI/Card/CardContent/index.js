04cf0b64983851d205c2d4d69d61783362c5ccd0
Iris Huang <lyaui@outloook.com>
2021-09-27 14:59:04 +0800

    remove CardContent contentEditable attribute

--- client/src/components/UI/Card/CardContent/index.js
+++ client/src/components/UI/Card/CardContent/index.js
@@ -1,7 +1,7 @@
 import { SCardContent } from 'components/UI/Card/CardContent/style.js';
 
 function CardContent({ children }) {
-  return <SCardContent contentEditable='true'>{children}</SCardContent>;
+  return <SCardContent>{children}</SCardContent>;
 }
 
 export default CardContent;
