676d9fcc5727d5666657ff1f02a67baa651835f4
Iris Huang <lyaui@outloook.com>
2021-09-28 04:40:24 +0800

    refactor CardLinks

--- client/src/components/UI/Card/CardLinks/index.js
+++ client/src/components/UI/Card/CardLinks/index.js
@@ -1,43 +1,30 @@
 import * as Icon from 'components/UI/Icon.js';
 import { ButtonRound } from 'components/UI/Buttons/index.js';
-import {
-  SCardLinks,
-  SCardLink,
-  SCardLinkImg,
-  SCardLinkInfo,
-  SCardLinkTitle,
-  SCardLinkUrl,
-  SCardLinkShare,
-  SCardLinkMore,
-} from 'components/UI/Card/CardLinks/style.js';
+import LinkItem from 'components/UI/LinkItem';
+import { SCardLink, SCardLinkMore } from 'components/UI/Card/CardLinks/style.js';
 
 const CardLinks = ({ links, isOnlyLinks }) => {
   const showLinksNum = 3;
   const numOfMoreLink = links.length - showLinksNum;
 
-  const goShareLink = (url) => {
+  const goShareLink = (url) => () => {
     window.open(`https://${url}`, '_blank').focus();
   };
 
   return (
-    <SCardLinks>
+    <div>
       {links.slice(0, showLinksNum).map((link, index) => (
-        <SCardLink key={index} index={index} isOnlyLinks={isOnlyLinks}>
-          <SCardLinkImg src={link.image} alt={link.title} isOnlyLinks={isOnlyLinks} />
-          <SCardLinkInfo isOnlyLinks={isOnlyLinks}>
-            <SCardLinkTitle>{link.title}</SCardLinkTitle>
-            <SCardLinkUrl>{link.url}</SCardLinkUrl>
-          </SCardLinkInfo>
-          <SCardLinkShare>
-            <ButtonRound size={28} onClick={() => goShareLink(link.url)}>
+        <SCardLink index={index} isOnlyLinks={isOnlyLinks}>
+          <LinkItem key={index} link={link} isOnlyLinks={isOnlyLinks}>
+            {/* go share link */}
+            <ButtonRound size={28} onClick={goShareLink(link.url)}>
               <Icon.Share />
             </ButtonRound>
-          </SCardLinkShare>
+          </LinkItem>
         </SCardLink>
       ))}
-
       {numOfMoreLink > 0 && <SCardLinkMore>還有個 {numOfMoreLink} 連結</SCardLinkMore>}
-    </SCardLinks>
+    </div>
   );
 };
 
--- client/src/components/UI/Card/CardLinks/style.js
+++ client/src/components/UI/Card/CardLinks/style.js
@@ -1,52 +1,10 @@
 import styled from 'styled-components/macro';
+import { SLinkItem } from 'components/UI/LinkItem/style.js';
 
-export const SCardLinks = styled.div``;
-
-export const SCardLink = styled.a`
-  display: flex;
+export const SCardLink = styled(SLinkItem)`
   height: ${(props) => (props.isOnlyLinks ? '88px' : '56px')};
   border-top: ${(props) =>
     props.index === 0 && props.isOnlyLinks ? 'unset' : '1px solid #cccccc'};
-  overflow: hidden;
-  background-color: rgba(250, 250, 250, 0.8);
-`;
-
-export const SCardLinkImg = styled.img`
-  width: ${(props) => (props.isOnlyLinks ? '88px' : '56px')};
-  background-color: #fff;
-  object-fit: cover;
-`;
-
-export const SCardLinkInfo = styled.div`
-  width: ${(props) =>
-    props.isOnlyLinks ? 'calc(100% - 88px - 32px)' : 'calc(100% - 56px - 32px)'};
-  padding: 8px 0 8px 8px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`;
-
-export const SCardLinkTitle = styled.div`
-  color: #202124;
-  font-weight: bold;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-`;
-
-export const SCardLinkUrl = styled.span`
-  color: #80868b;
-  margin-top: 4px;
-  font-size: 12px;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-`;
-
-export const SCardLinkShare = styled.div`
-  &button {
-    margin-right: 20px;
-  }
 `;
 
 export const SCardLinkMore = styled.div`
