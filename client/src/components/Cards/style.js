e3289ad4fea07803fe5d7911e6166ee768cba744
Iris Huang <lyaui@outloook.com>
2021-09-27 15:44:05 +0800

    change cards space style

--- client/src/components/Cards/style.js
+++ client/src/components/Cards/style.js
@@ -3,10 +3,10 @@ import { VIEW_MODE } from 'constants/UI.js';
 
 export const SCards = styled.section`
   display: grid;
-  grid-gap: ${(props) => (props.viewMode === VIEW_MODE.GRID ? '8px' : '20px')};
+  grid-gap: ${(props) => (props.viewMode === VIEW_MODE.GRID ? '12px' : '20px')};
   grid-template-columns: ${(props) =>
     props.viewMode === VIEW_MODE.GRID ? 'repeat(auto-fill, minmax(210px, 1fr))' : '600px'};
   justify-content: center;
   grid-auto-rows: 0;
-  padding: 20px 60px;
+  padding: 20px 100px;
 `;
