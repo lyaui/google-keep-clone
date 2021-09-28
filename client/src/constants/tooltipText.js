24006f3018bc09f248837f4ce197ea1e3516e866
Iris Huang <lyaui@outloook.com>
2021-09-28 18:19:57 +0800

    add constant tooltipText

---
+++ client/src/constants/tooltipText.js
@@ -0,0 +1,26 @@
+export const TOOLTIP_TEXT = {
+  MENU: '主選單',
+  SEARCH: '搜尋',
+  CLEAR_SEARCH: '清除搜尋內容',
+  PIN: '固定記事',
+  CANCEL_PIN: '取消固定記事',
+  REMOVE: '移除',
+  GO_URL: '前往連結',
+  PREVIEW_URL: '連結預覽選項',
+  VIEW_GRID: '格狀檢視',
+  VIEW_LIST: '清單檢視',
+  DARK_MODE: '啟用深色主題',
+  LIGHT_MODE: '停用深色主題',
+  LOGOUT: '登出',
+
+  // toolbar
+  PALETTE: '變更顏色',
+  LABEL: '新增標籤',
+  CANCEL_LABEL: '移除標籤',
+  CHECKBOX: '顯示核取方塊',
+  CANCEL_CHECKBOX: '隱藏核取方塊',
+  IMAGE: '新增圖片',
+  COPY: '建立副本',
+  ARCHIVE: '封存',
+  CANCEL_ARCHIVE: '取消封存',
+};
