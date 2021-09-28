9819518ecd7b98e27aba585a086e1c7e5e320b12
Iris Huang <lyaui@outloook.com>
2021-09-29 03:26:35 +0800

    add CustomTooltip

--- client/src/components/UI/CustomTooltip/index.js
+++ client/src/components/UI/CustomTooltip/index.js
@@ -1,20 +1,21 @@
 import CustomTippy from '@tippyjs/react/headless';
+import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
 import { useUIContextVal } from 'contexts/ui-context.js';
 
-const SettingTooltip = ({ children, renderElement }) => {
+const CustomTooltip = ({ children, renderElement = null, name = null, text = '' }) => {
   const { CTX_TOOLTIP } = useUIContextVal();
 
   return (
     <CustomTippy
       render={() => renderElement}
-      visible={CTX_TOOLTIP.isTooltipVisible}
+      visible={CTX_TOOLTIP.tooltipName === name}
       interactive={true}
       onClickOutside={CTX_TOOLTIP.hideTooltipHandler}
     >
-      {children}
+      <Tippy content={text}>{children}</Tippy>
     </CustomTippy>
   );
 };
 
-export default SettingTooltip;
+export default CustomTooltip;
