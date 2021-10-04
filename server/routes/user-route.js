e598792c89d0ad0edaa146dcdf84e2ede96f20dc
Iris Huang <lyaui@outloook.com>
2021-10-04 08:32:40 +0800

    add google jwt token

--- server/routes/user-route.js
+++ server/routes/user-route.js
@@ -1,8 +1,29 @@
 const router = require('express').Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 const { userController } = require('../controllers');
 
-router.post('/signup', userController.signup);
-router.post('/login', userController.login);
+router.post('/signup', passport.authenticate('jwt', { session: false }), userController.signup);
+router.post('/login', passport.authenticate('jwt', { session: false }), userController.login);
+router.get(
+  '/google',
+  passport.authenticate('google', { session: false, scope: ['profile', 'email'] }),
+);
+router.get('/google/redirect', passport.authenticate('google', { session: false }), (req, res) => {
+  const { user } = req;
+  const tokenObj = { id: user._id, email: user.email };
+  const token = jwt.sign(tokenObj, process.env.TOKEN_SECRET, { expiresIn: '30d' });
+  res.status(200).json({
+    success: true,
+    data: {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token,
+    },
+    message: 'Logined in!',
+  });
+});
 router.get('/settings/:userId', userController.getUserSettings);
 router.patch('/settings/:userId', userController.updateUserSettings);
 
