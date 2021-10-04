3904f4458f1d1839a1b8e9ce414aebd3fb244503
Iris Huang <lyaui@outloook.com>
2021-10-04 08:32:18 +0800

    add signup and login jwt token

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { HttpError, User } = require('../models');
 
-// TODO 等加入 user auth 直接確認 cretor 是否等於 req.user 並移除 check if user exists
 const signup = async (req, res, next) => {
   const { name, email, password } = req.body;
   try {
@@ -19,14 +19,17 @@ const signup = async (req, res, next) => {
       password: hashedPassword,
     }).save();
 
+    // token
+    const tokenObj = { id: createdUser._id, email: createdUser.email };
+    const token = jwt.sign(tokenObj, process.env.TOKEN_SECRET, { expiresIn: '30d' });
+
     res.status(201).json({
       success: true,
       data: {
         _id: createdUser._id,
         name: createdUser.name,
         email: createdUser.email,
-        settings: createdUser.settings,
-        labels: createdUser.labels,
+        token,
       },
       message: 'Signed up successfully.',
     });
@@ -46,14 +49,17 @@ const login = async (req, res, next) => {
     const isValidPassword = await bcrypt.compare(password, user.password);
     if (!isValidPassword) return next(new HttpError('Invalid credential, please try again.', 401));
 
+    // token
+    const tokenObj = { id: createdUser._id, email: createdUser.email };
+    const token = jwt.sign(tokenObj, process.env.TOKEN_SECRET, { expiresIn: '30d' });
+
     res.status(200).json({
       success: true,
       data: {
         _id: user._id,
         name: user.name,
         email: user.email,
-        settings: user.settings,
-        labels: user.labels,
+        token,
       },
       message: 'Logined in!',
     });
