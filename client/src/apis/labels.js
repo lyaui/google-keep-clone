af4fa52b0f79323bb9b4488a1d2055f24a5f9737
Iris Huang <lyaui@outloook.com>
2021-10-04 11:12:10 +0800

    fix labels delete api

--- client/src/apis/labels.js
+++ client/src/apis/labels.js
@@ -3,4 +3,4 @@ import { axiosLabels } from 'apis';
 export const apiGetLabels = () => axiosLabels.get('/');
 export const apiCreateLabel = (data) => axiosLabels.post('/', data);
 export const apiUpdateLabel = (data) => axiosLabels.patch('/:labelId', data);
-export const apiDeleteLabel = (data) => axiosLabels.delete('/:labelId', data);
+export const apiDeleteLabel = () => axiosLabels.delete('/:labelId');
