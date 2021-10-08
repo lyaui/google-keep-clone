1825d3b45f2f65228c325b905ab0afd06ad6f7af
Iris Huang <lyaui@outloook.com>
2021-10-09 03:23:00 +0800

    add toast text constants

--- client/src/constants/toastText.js
+++ client/src/constants/toastText.js
@@ -1,4 +1,14 @@
 export const TOAST_TEXT = {
+  // SIGN UP
+  SIGNUP_SUCCESS: '🥳 註冊成功 🎉🎉🎉',
+  SIGNUP_ACCOUNT_EXISTED: '🥶 使用者帳戶已存在',
+  SIGNUP_FAIL: '😓 註冊失敗 請重新嘗試',
+
+  // LOGIN
   LOGIN_SUCCESS: '🥳 登入成功',
   LOGIN_FAIL: '😓 登入失敗 請重新嘗試',
+
+  // LOGOUT
+  LOGOUT_SUCCESS: '👋 登出成功',
+  LOGOUT_FAIL: '🤭 登出失敗 請稍候再嘗試',
 };
