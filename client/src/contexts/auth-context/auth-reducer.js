7b917931918ad243c6421a9866cacf410464ba66
Iris Huang <lyaui@outloook.com>
2021-10-09 03:19:30 +0800

    add auth reducers

---
+++ client/src/contexts/auth-context/auth-reducer.js
@@ -0,0 +1,41 @@
+import { INIT_AUTH_STATES, AUTH_TYPES } from 'constants/auth.js';
+
+export const authReducer = (state = INIT_AUTH_STATES, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case AUTH_TYPES.SIGNUP_REQUEST:
+    case AUTH_TYPES.LOGIN_REQUEST:
+    case AUTH_TYPES.GOOGLE_LOGIN_REQUEST:
+    case AUTH_TYPES.LOGOUT_REQUEST:
+      return { ...state, isLoading: true, errorMessage: '' };
+
+    case AUTH_TYPES.SIGNUP_SUCCESS:
+    case AUTH_TYPES.LOGIN_SUCCESS:
+    case AUTH_TYPES.GOOGLE_LOGIN_SUCCESS:
+      return {
+        ...state,
+        isLoggedIn: true,
+        isLoading: false,
+        userId: payload.userId,
+        userName: payload.userName,
+        userEmail: payload.userEmail,
+        token: payload.token,
+      };
+
+    case AUTH_TYPES.LOGOUT_SUCCESS:
+      return {
+        ...state,
+        ...INIT_AUTH_STATES,
+      };
+
+    case AUTH_TYPES.SIGNUP_FAIL:
+    case AUTH_TYPES.LOGIN_FAIL:
+    case AUTH_TYPES.GOOGLE_LOGIN_FAIL:
+    case AUTH_TYPES.LOGOUT_FAIL:
+      return { ...state, isLoading: false, errorMessage: payload.errorMessage };
+
+    default:
+      return state;
+  }
+};
