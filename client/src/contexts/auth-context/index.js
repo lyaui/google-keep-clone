6c229535f676e32596c4c8d54b758aac50a6b9f1
Iris Huang <lyaui@outloook.com>
2021-10-12 02:05:28 +0800

    fix auth context

--- client/src/contexts/auth-context/index.js
+++ client/src/contexts/auth-context/index.js
@@ -1,13 +1,17 @@
 import { createContext, useReducer, useContext } from 'react';
 import { INIT_AUTH_STATES } from 'constants/auth.js';
-import { signup, login, googleLogin, logout } from 'contexts/auth-context/auth-action.js';
+import { signup, login, logout } from 'contexts/auth-context/auth-action.js';
 import { authReducer } from 'contexts/auth-context/auth-reducer.js';
 
-const AuthContext = createContext({ authState: INIT_AUTH_STATES, authDispatch() {} });
+const initAuthStates = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : INIT_AUTH_STATES;
+
+const AuthContext = createContext({ authState: initAuthStates, authDispatch() {} });
 
 // export Provider
 export const AuthProvider = ({ children }) => {
-  const [authState, authDispatch] = useReducer(authReducer, INIT_AUTH_STATES);
+  const [authState, authDispatch] = useReducer(authReducer, initAuthStates);
   const contextValue = { authState, authDispatch };
   return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>;
 };
@@ -16,4 +20,4 @@ export const AuthProvider = ({ children }) => {
 export const useAuth = () => useContext(AuthContext);
 
 // export actions
-export { signup, login, googleLogin, logout };
+export { signup, login, logout };
