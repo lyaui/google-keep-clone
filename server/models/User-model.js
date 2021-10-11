4edce290fbce6c327ebfc68f841ed7b1718c2bdf
Iris Huang <lyaui@outloook.com>
2021-10-12 02:15:43 +0800

    fix user settings model

--- server/models/User-model.js
+++ server/models/User-model.js
@@ -9,9 +9,10 @@ const userSchema = new Schema({
   memos: [{ type: Schema.Types.ObjectId, ref: 'Memo', default: [] }],
   labels: [{ type: Schema.Types.ObjectId, ref: 'Label', default: [] }],
   settings: {
-    theme: { type: String, required: true, enum: ['LIGHT', 'DARK'], default: 'LIGHT' },
-    layout: { type: String, required: true, enum: ['GRID', 'LIST'], default: 'GRID' },
-    sort: { type: String, required: true, enum: ['ASCEND', 'DESCEND'], default: 'DESCEND' },
+    theme: { type: String, enum: ['LIGHT', 'DARK'], default: 'LIGHT' },
+    layout: { type: String, enum: ['GRID', 'LIST'], default: 'GRID' },
+    sort: { type: String, enum: ['ASCEND', 'DESCEND'], default: 'DESCEND' },
+    isFixedMenu: { type: Boolean, default: true },
   },
 });
 const User = model('User', userSchema);
