1cb694422cc7ee44149f4933df8a3ff033e76474
Iris Huang <lyaui@outloook.com>
2021-10-12 02:08:17 +0800

    add getUserSettings

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,8 +1,35 @@
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { TOAST_TEXT } from 'constants/toastText.js';
+import { ROUTE } from 'constants/routes.js';
+import { useAuth } from 'contexts/auth-context';
+import { useUI, getUserSettings } from 'contexts/UI-context/index.js';
 import Layout from 'components/Layout';
 import AddNewCard from 'components/EditCard';
 import Cards from 'components/Cards';
 
-const main = () => {
+const Main = () => {
+  const history = useHistory();
+  const { authState } = useAuth();
+  const { UIState, UIDispatch } = useUI();
+  const { isLoading } = UIState;
+  const userId = authState.userId;
+
+  useEffect(() => {
+    (async () => {
+      if (!userId) return history.replace(ROUTE.LOGIN);
+
+      try {
+        const res = await getUserSettings(UIDispatch, { userId });
+        if (!res.success) throw new Error();
+      } catch {
+        toast(TOAST_TEXT.SETTINGS_FAIL);
+        history.replace(ROUTE.LOGIN);
+      }
+    })();
+  }, [history, UIDispatch, userId]);
+
   return (
     <Layout>
       <AddNewCard />
@@ -11,4 +38,4 @@ const main = () => {
   );
 };
 
-export default main;
+export default Main;
