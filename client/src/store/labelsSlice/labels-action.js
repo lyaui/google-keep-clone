3bed0aa61a16b1d0189dc34e557e1c63ce87f92a
Iris Huang <lyaui@outloook.com>
2021-10-14 02:26:34 +0800

    add addLabel action

--- client/src/store/labelsSlice/labels-action.js
+++ client/src/store/labelsSlice/labels-action.js
@@ -1,11 +1,24 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { apiGetLabels } from 'apis/labels.js';
+import { apiGetLabels, apiCreateLabel } from 'apis/labels.js';
 
 export const getUserLabels = createAsyncThunk(
   'labels/getLabels',
   async (userId, { rejectWithValue }) => {
     try {
       const res = await apiGetLabels(userId);
+      if (!res.data.success) throw new Error();
+      return res.data.labels;
+    } catch (err) {
+      return rejectWithValue(err.response.data.message);
+    }
+  },
+);
+
+export const addLabel = createAsyncThunk(
+  'labels/addLabels',
+  async (payload, { rejectWithValue }) => {
+    try {
+      const res = await apiCreateLabel(payload);
       if (!res.data.success) throw new Error();
       return res.data.labels;
     } catch (err) {
