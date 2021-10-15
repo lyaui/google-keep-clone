3ccf6b41f5c854618a14ee609ce8786c6d89a7bb
Iris Huang <lyaui@outloook.com>
2021-10-15 10:15:16 +0800

    add memoReducer into redux store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import labelsReducer from 'store/labelsSlice';
+import memosReducer from 'store/memosSlice';
 
 const store = configureStore({
   reducer: {
     labels: labelsReducer,
+    memos: memosReducer,
   },
 });
 export default store;
