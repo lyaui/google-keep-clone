fdb79f3972e79f2d16cc96e199ad7ce16960347a
Iris Huang <lyaui@outloook.com>
2021-10-15 10:16:02 +0800

    add cards dispatch  getUserMemos action

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -1,21 +1,31 @@
 import { useState, useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Droppable } from 'react-beautiful-dnd';
 import composeRefs from '@seznam/compose-react-refs';
-import { useMemoContextVal } from 'contexts/memo-context.js';
-import { useUI } from 'contexts/UI-context/index.js';
+
+import { getUserMemos } from 'store/memosSlice/memos-action.js';
+import { useAuth } from 'contexts/auth-context';
+import { useUI } from 'contexts/UI-context';
 import Card from 'components/UI/Card';
 import { SCards } from 'components/Cards/style.js';
 
 const Cards = () => {
-  const { memos } = useMemoContextVal();
+  const dispatch = useDispatch();
+  const { memos } = useSelector((state) => state.memos);
+
+  console.log({ memos });
+
+  const { authState } = useAuth();
   const { UIState } = useUI();
+  const { userId } = authState;
   const { layout, isFixedMenu } = UIState;
   const masonryRef = useRef();
   const [masonryDom, setMasonryDom] = useState(null);
 
   useEffect(() => {
+    dispatch(getUserMemos(userId));
     setMasonryDom(masonryRef.current);
-  }, []);
+  }, [dispatch, setMasonryDom, userId]);
 
   return (
     <Droppable droppableId='cards'>
