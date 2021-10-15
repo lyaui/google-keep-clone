8c76030d8801ec0491e4ad126e58af6c234521ee
Iris Huang <lyaui@outloook.com>
2021-10-15 10:17:59 +0800

    remove memos context

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Toast from 'components/UI/Toast';
 import { DragDropContext } from 'react-beautiful-dnd';
 import { ROUTE } from 'constants/routes.js';
-import MemoContextProvider, { useMemoContextVal } from 'contexts/memo-context.js';
 import { AuthProvider, useAuth } from 'contexts/auth-context';
 import { UIProvider } from 'contexts/UI-context/index.js';
 import Main from 'pages/Main.js';
@@ -11,10 +10,8 @@ import Login from 'pages/Login.js';
 function App() {
   const { authState } = useAuth();
   const { isLoggedIn } = authState;
-  const { setMemos } = useMemoContextVal();
 
   const onDragEnd = () => {
-    setMemos([{ id: 'c12', color: '#e8eaed', title: 'gray', content: 'gray' }]);
     // const { draggableId, source, destination } = result;
     // // 若不在 droppable 的範圍內
     // if (!destination) return;
@@ -29,20 +26,18 @@ function App() {
     <DragDropContext onDragEnd={onDragEnd}>
       <AuthProvider>
         <UIProvider>
-          <MemoContextProvider>
-            <main>
-              <Switch>
-                <Route path={[ROUTE.LOGIN, ROUTE.SIGNUP]}>
-                  <Login />
-                </Route>
-                <Route path={[ROUTE.LABEL, ROUTE.HOME]}>
-                  <Main />
-                </Route>
-                <Redirect to={ROUTE.LOGIN} />
-              </Switch>
-            </main>
-            <Toast />
-          </MemoContextProvider>
+          <main>
+            <Switch>
+              <Route path={[ROUTE.LOGIN, ROUTE.SIGNUP]}>
+                <Login />
+              </Route>
+              <Route path={[ROUTE.LABEL, ROUTE.HOME]}>
+                <Main />
+              </Route>
+              <Redirect to={ROUTE.LOGIN} />
+            </Switch>
+          </main>
+          <Toast />
         </UIProvider>
       </AuthProvider>
     </DragDropContext>
