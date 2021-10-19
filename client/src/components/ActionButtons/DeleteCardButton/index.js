a8b8b3f7c0fd1b5d6a170b9d55b442ce311c02e5
Iris Huang <lyaui@outloook.com>
2021-10-19 12:00:44 +0800

    add DeleteCardButton loading status

--- client/src/components/ActionButtons/DeleteCardButton/index.js
+++ client/src/components/ActionButtons/DeleteCardButton/index.js
@@ -1,4 +1,4 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
 import { TOOLTIP_TEXT } from 'constants/tooltipText.js';
@@ -8,6 +8,7 @@ import { ButtonRound } from 'components/UI/Buttons/index.js';
 
 const DeleteCardButton = ({ id }) => {
   const dispatch = useDispatch();
+  const { isLoading } = useSelector((state) => state.memos);
 
   const deleteMemoHandler = (e) => {
     e.stopPropagation();
@@ -17,7 +18,7 @@ const DeleteCardButton = ({ id }) => {
 
   return (
     <Tippy content={TOOLTIP_TEXT.DELETE_MEMO}>
-      <ButtonRound onClick={deleteMemoHandler} size={34}>
+      <ButtonRound onClick={deleteMemoHandler} size={34} disabled={isLoading}>
         <Icon.DeleteOutline />
       </ButtonRound>
     </Tippy>
