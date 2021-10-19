a8ec07a42b45eb62961f843569d0873da12fa872
Iris Huang <lyaui@outloook.com>
2021-10-19 13:09:30 +0800

    fix memo schema

--- server/models/Memo-model.js
+++ server/models/Memo-model.js
@@ -1,24 +1,30 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
+const linkSchema = new Schema({
+  title: { type: String, default: '' },
+  url: { type: String, default: '', required: true },
+  image: { type: String, default: '' },
+});
+
+const taskSchema = new Schema({
+  name: { type: String, default: '', required: true },
+  isCompleted: { type: Boolean, default: false, required: true },
+  id: { type: String, default: '', required: true },
+});
+
 const memoSchema = new Schema(
   {
     creator: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     title: { type: String, default: '' },
     content: { type: String, default: '' },
-    images: [{ type: String, default: [] }],
+    images: [{ type: String, default: '' }],
     isTaskList: { type: Boolean, default: false },
     isPinned: { type: Boolean, default: false },
     isArchived: { type: Boolean, default: false },
-    links: [{ type: String, default: [] }],
-    labels: [{ type: Schema.Types.ObjectId, ref: 'Label', default: [] }],
-    tasks: [
-      {
-        task: { type: String, default: '' },
-        isCompleted: { type: Boolean, default: false },
-        default: [],
-      },
-    ],
+    links: [linkSchema],
+    labels: [{ type: Schema.Types.ObjectId, ref: 'Label' }],
+    tasks: [taskSchema],
     color: {
       type: String,
       enum: [
