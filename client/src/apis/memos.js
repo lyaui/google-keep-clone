67438214999a6af25f0d1fd4091a21d308bfc9c8
Iris Huang <lyaui@outloook.com>
2021-10-20 13:23:28 +0800

    change memos api routes

--- client/src/apis/memos.js
+++ client/src/apis/memos.js
@@ -1,7 +1,7 @@
 import { axiosMemos } from 'apis';
 
-export const apiGetMemos = (userId) => axiosMemos.get(`/user/${userId}`);
-export const apiGetMemosByLabelId = (labelId) => axiosMemos.get(`/label/${labelId}`);
+export const apiGetUserMemos = () => axiosMemos.get('/');
+export const apiGetMemosByLabelName = (labelName) => axiosMemos.get(`/label/${labelName}`);
 export const apiCreateMemo = (data) => axiosMemos.post('/', data);
 export const apiUpdateMemo = (memoId, data) => axiosMemos.patch(`/${memoId}`, data);
 export const apiDeleteMemo = (memoId) => axiosMemos.delete(`/${memoId}`);
--- server/routes/memos-route.js
+++ server/routes/memos-route.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 const { memoController } = require('../controllers');
+const fileUpload = require('../middleware/file-upload.js');
 
-router.get('/user/:userId', memoController.getMemos);
-router.get('/label/:labelId', memoController.getMemosByLabelId);
+router.get('/', memoController.getUserMemos);
+router.get('/label/:labelName', memoController.getUserMemosByLabelName);
 router.post('/', memoController.createMemo);
 router.patch('/:memoId', memoController.updateMemo);
 router.delete('/:memoId', memoController.deleteMemo);
+router.post('/singleImage', fileUpload.single('image'), memoController.uploadImage);
+router.post('/multipleImages', fileUpload.array('images', 5), memoController.uploadImages);
 
 module.exports = router;
