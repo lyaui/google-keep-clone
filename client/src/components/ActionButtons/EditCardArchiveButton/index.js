13951fe89b58e9869028d84ec0ca83c8bca22850
Iris Huang <lyaui@outloook.com>
2021-10-24 05:42:30 +0800

    refactor EditCardArchiveButton

--- client/src/components/ActionButtons/EditCardArchiveButton/index.js
+++ client/src/components/ActionButtons/EditCardArchiveButton/index.js
@@ -1,6 +1,4 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { memosActions } from 'store/memosSlice';
-import { updateMemo } from 'store/memosSlice/memos-action.js';
+import { useUpdateMemo } from 'hooks/updateMemo-hook.js';
 import * as Icon from 'components/UI/Icon/index.js';
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
@@ -8,15 +6,11 @@ import { TOOLTIP_TEXT } from 'constants/tooltipText.js';
 import { ButtonRound } from 'components/UI/Buttons/index.js';
 
 const EditCardArchiveButton = ({ id }) => {
-  const dispatch = useDispatch();
-  const { memo, memos, isEditingNewMemo } = useSelector((state) => state.memos);
-  const currentMemo = memos.find((memo) => memo._id === id) || memo;
+  const { currentMemo, dispatchUpdateMemo } = useUpdateMemo(id);
 
   const toggleArchiveHandler = (e) => {
     e.stopPropagation();
-    isEditingNewMemo
-      ? dispatch(memosActions.updateMemo({ isArchived: !currentMemo.isArchived }))
-      : dispatch(updateMemo({ memoId: id, payload: { isArchived: !currentMemo.isArchived } }));
+    dispatchUpdateMemo({ isArchived: !currentMemo.isArchived });
   };
 
   const tooltipText = currentMemo.isArchived ? TOOLTIP_TEXT.CANCEL_ARCHIVE : TOOLTIP_TEXT.ARCHIVE;
