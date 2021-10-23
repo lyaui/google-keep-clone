34bc4742a130dedf905a1770151cd41e21f75d7a
Iris Huang <lyaui@outloook.com>
2021-10-24 05:31:05 +0800

    add useUpdateMemo

---
+++ client/src/hooks/updateMemo-hook.js
@@ -0,0 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { memosActions } from 'store/memosSlice';
+import { updateMemo } from 'store/memosSlice/memos-action.js';
+
+export const useUpdateMemo = (id) => {
+  const dispatch = useDispatch();
+  const { memos, memo, isLoading } = useSelector((state) => state.memos);
+  const currentMemo = memos.find((memo) => memo._id === id) || memo;
+
+  const dispatchUpdateMemo = (updatedItem) => {
+    console.log(id, updatedItem);
+    if (isLoading) return;
+    id
+      ? dispatch(updateMemo({ memoId: id, payload: updatedItem }))
+      : dispatch(memosActions.updateMemo(updatedItem));
+  };
+
+  return { currentMemo, dispatchUpdateMemo };
+};
