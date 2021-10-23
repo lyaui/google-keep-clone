455b966f8a108505fe1c8fc87ea8e7393ecd84f8
Iris Huang <lyaui@outloook.com>
2021-10-24 05:48:31 +0800

    refactor Label

--- client/src/components/UI/Label/index.js
+++ client/src/components/UI/Label/index.js
@@ -1,6 +1,4 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { memosActions } from 'store/memosSlice';
-import { updateMemo } from 'store/memosSlice/memos-action.js';
+import { useUpdateMemo } from 'hooks/updateMemo-hook.js';
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
 import { TOOLTIP_TEXT } from 'constants/tooltipText.js';
@@ -8,17 +6,12 @@ import * as Icon from 'components/UI/Icon/index.js';
 import { SLabel, SLabelButton } from 'components/UI/Label/style.js';
 
 const Label = ({ id, label, isShowMoreLabel = false, numOfMoreLabel = 0 }) => {
-  const dispatch = useDispatch();
-  const { memos, memo, isLoading } = useSelector((state) => state.memos);
-  const currentMemo = memos.find((memo) => memo._id === id) || memo;
+  const { currentMemo, dispatchUpdateMemo } = useUpdateMemo(id);
+  const updatedLabels = currentMemo.labels.filter((item) => item._id !== label._id);
 
   const removeLabelHandler = (e) => {
     e.stopPropagation();
-    const updatedLabels = currentMemo.labels.filter((item) => item._id !== label._id);
-    if (isLoading) return;
-    id
-      ? dispatch(updateMemo({ memoId: id, payload: { labels: updatedLabels } }))
-      : dispatch(memosActions.updateMemo({ labels: updatedLabels }));
+    dispatchUpdateMemo({ labels: updatedLabels });
   };
 
   return (
