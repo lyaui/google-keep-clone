7ef1ffc8a8db2f9ff2078f2f5da20418238224b5
Iris Huang <lyaui@outloook.com>
2021-10-24 05:31:59 +0800

    refactor MemoLabel

--- client/src/components/EditLabels/MemoLabel/index.js
+++ client/src/components/EditLabels/MemoLabel/index.js
@@ -1,27 +1,18 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { memosActions } from 'store/memosSlice';
-import { updateMemo } from 'store/memosSlice/memos-action.js';
+import { useUpdateMemo } from 'hooks/updateMemo-hook.js';
 import * as Icon from 'components/UI/Icon/index.js';
 import { SLabel, SLabelIcon, SLabelValue } from 'components/EditLabels/style.js';
 
 const MemoLabel = ({ id, label, isSideMenu }) => {
-  const dispatch = useDispatch();
-  const { memo, memos, isEditingNewMemo, isLoading } = useSelector((state) => state.memos);
-  const currentMemo = memos.find((memo) => memo._id === id) || memo;
+  const { currentMemo, dispatchUpdateMemo } = useUpdateMemo(id);
   const isSelected = !!currentMemo.labels.find((item) => item._id === label._id);
+
+  const updatedLabels = isSelected
+    ? currentMemo.labels.filter((item) => item._id !== label._id)
+    : [...currentMemo.labels, label];
 
   const toggleLabelHandler = (e) => {
     e.preventDefault();
-    if (isLoading) return;
-
-    // add or remove label
-    const updatedLabels = isSelected
-      ? currentMemo.labels.filter((item) => item._id !== label._id)
-      : [...currentMemo.labels, label];
-
-    isEditingNewMemo
-      ? dispatch(memosActions.updateMemo({ labels: updatedLabels }))
-      : dispatch(updateMemo({ memoId: id, payload: { labels: updatedLabels } }));
+    dispatchUpdateMemo({ labels: updatedLabels });
   };
 
   return (
