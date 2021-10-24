a438fddd3e1f1345dc358fc9e8e02a88174556af
Iris Huang <lyaui@outloook.com>
2021-10-25 04:50:56 +0800

    remove upload image button from Cardfooter

--- client/src/components/UI/Card/CardFooter/index.js
+++ client/src/components/UI/Card/CardFooter/index.js
@@ -1,8 +1,3 @@
-import * as Icon from 'components/UI/Icon/index.js';
-import Tippy from '@tippyjs/react';
-import 'tippy.js/dist/tippy.css';
-import { TOOLTIP_TEXT } from 'constants/tooltipText.js';
-import { ButtonRound } from 'components/UI/Buttons/index.js';
 import EditCardColorButton from 'components/ActionButtons/EditCardColorButton';
 import EditCardLabelsButton from 'components/ActionButtons/EditCardLabelsButton';
 import CopyMemoButton from 'components/ActionButtons/CopyMemoButton';
@@ -10,12 +5,8 @@ import EditCardArchiveButton from 'components/ActionButtons/EditCardArchiveButto
 import DeleteCardButton from 'components/ActionButtons/DeleteCardButton';
 import { SCardFooter } from 'components/UI/Card/CardFooter/style.js';
 
-function CardFooter({ id, isOnlyImages, isOnlyLinks, isOnlyImagesAndLinks }) {
+const CardFooter = ({ id, isOnlyImages, isOnlyLinks, isOnlyImagesAndLinks }) => {
   const isOnlyImagesOrLinks = isOnlyImages || isOnlyLinks;
-  const test = (e) => {
-    e.stopPropagation();
-    alert('alert');
-  };
   return (
     <SCardFooter
       isOnlyImagesOrLinks={isOnlyImagesOrLinks}
@@ -25,12 +16,6 @@ function CardFooter({ id, isOnlyImages, isOnlyLinks, isOnlyImagesAndLinks }) {
       <EditCardColorButton id={id} />
       {/* labels */}
       <EditCardLabelsButton id={id} />
-      {/* image */}
-      <Tippy content={TOOLTIP_TEXT.IMAGE}>
-        <ButtonRound onClick={test} size={34}>
-          <Icon.Image />
-        </ButtonRound>
-      </Tippy>
       {/* copy */}
       <CopyMemoButton id={id} />
       {/* archive */}
@@ -39,6 +24,6 @@ function CardFooter({ id, isOnlyImages, isOnlyLinks, isOnlyImagesAndLinks }) {
       <DeleteCardButton id={id} />
     </SCardFooter>
   );
-}
+};
 
 export default CardFooter;
