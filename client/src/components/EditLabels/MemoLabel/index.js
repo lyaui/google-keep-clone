6b6c63445ca9357085202a5882f0eef910bdcc11
Iris Huang <lyaui@outloook.com>
2021-10-26 02:51:53 +0800

    add label by params

--- client/src/components/EditLabels/MemoLabel/index.js
+++ client/src/components/EditLabels/MemoLabel/index.js
@@ -1,17 +1,27 @@
+import { useEffect } from 'react';
+import { useRouteMatch } from 'react-router-dom';
 import { useUpdateMemo } from 'hooks/updateMemo-hook.js';
 import * as Icon from 'components/UI/Icon/index.js';
 import { SLabel, SLabelIcon, SLabelValue } from 'components/EditLabels/style.js';
 
 const MemoLabel = ({ id, label, isSideMenu }) => {
-  const { currentMemo, dispatchUpdateMemo } = useUpdateMemo(id);
-  const isSelected = !!currentMemo.labels.find((item) => item._id === label._id);
+  const { params } = useRouteMatch();
+  const { labelName } = params;
 
-  const updatedLabels = isSelected
-    ? currentMemo.labels.filter((item) => item._id !== label._id)
-    : [...currentMemo.labels, label];
+  const { currentMemo, dispatchUpdateMemo } = useUpdateMemo(id);
+  const isSelected = !!currentMemo.labels.find((item) => item._id === label._id) || false;
+
+  useEffect(() => {
+    if (id) return;
+    if (label.name !== labelName) return;
+    dispatchUpdateMemo({ labels: [...currentMemo.labels, label] });
+  }, []);
 
   const toggleLabelHandler = (e) => {
     e.preventDefault();
+    const updatedLabels = isSelected
+      ? currentMemo.labels.filter((item) => item._id !== label._id)
+      : [...currentMemo.labels, label];
     dispatchUpdateMemo({ labels: updatedLabels });
   };
 
