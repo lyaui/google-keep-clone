8e72bf0fae2a179df791fdc21e3730c665733a07
Iris Huang <lyaui@outloook.com>
2021-10-25 23:25:05 +0800

    add apiGetLinkInfo api

--- client/src/apis/memos.js
+++ client/src/apis/memos.js
@@ -6,4 +6,4 @@ export const apiGetUserMemoByMemoId = (memoId) => axiosMemos.get(`/${memoId}`);
 export const apiCreateMemo = (data) => axiosMemos.post('/', data);
 export const apiUpdateMemo = (memoId, data) => axiosMemos.patch(`/${memoId}`, data);
 export const apiDeleteMemo = (memoId) => axiosMemos.delete(`/${memoId}`);
-export const apiGetLinkInfo = (data) => axiosMemos.post('/linksInfo', data);
+export const apiGetLinksInfo = (data) => axiosMemos.post('/linksInfo', data);
