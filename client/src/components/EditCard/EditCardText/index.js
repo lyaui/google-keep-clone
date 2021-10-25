0affc84f00ef8d3d6064e8fcfba9bf2e47df722a
Iris Huang <lyaui@outloook.com>
2021-10-25 23:44:11 +0800

    add EditCardText dispatch addLinksInfo

--- client/src/components/EditCard/EditCardText/index.js
+++ client/src/components/EditCard/EditCardText/index.js
@@ -1,9 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useLocation, useRouteMatch } from 'react-router-dom';
 import Autolinker from 'autolinker';
 import ContentEditable from 'react-contenteditable';
+import { ROUTE } from 'constants/routes.js';
+import { addLinksInfo } from 'store/memosSlice/memos-action.js';
 
 const EditCardText = ({ text, updateTextHandler }) => {
+  const dispatch = useDispatch();
+  const { path } = useRouteMatch();
+  const { search } = useLocation();
+  const editQuery = !!new URLSearchParams(search).get('edit');
   const [links, setLinks] = useState([]);
+
+  useEffect(() => {
+    if (links.length === 0) return;
+    if (editQuery || path === ROUTE.MEMO) {
+      (async () => {
+        await dispatch(addLinksInfo({ links }));
+      })();
+    }
+  }, [links, editQuery, path, dispatch]);
 
   const textChangeHandler = (e) => {
     const linkedText = Autolinker.link(e.target.value, {
@@ -12,7 +29,7 @@ const EditCardText = ({ text, updateTextHandler }) => {
     const matches = Autolinker.parse(e.target.value, {
       urls: true,
     });
-    setLinks(matches.map((link) => link.url));
+    setLinks((preState) => [...preState, ...matches.map((link) => link.url)]);
     updateTextHandler(linkedText);
   };
 
