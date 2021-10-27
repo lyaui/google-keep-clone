1773bf1e4af8aefed6843b26aa196f0c40ff0b3e
Iris Huang <lyaui@outloook.com>
2021-10-27 09:15:02 +0800

    remove upload middleware from memo routes

--- server/routes/memos-route.js
+++ server/routes/memos-route.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { memoController } = require('../controllers');
-const fileUpload = require('../middleware/file-upload.js');
 
 router.get('/', memoController.getUserMemos);
 router.get('/label/:labelName', memoController.getUserMemosByLabelName);
@@ -8,8 +7,6 @@ router.get('/:memoId', memoController.getUserMemoByMemoId);
 router.post('/', memoController.createMemo);
 router.patch('/:memoId', memoController.updateMemo);
 router.delete('/:memoId', memoController.deleteMemo);
-router.post('/singleImage', fileUpload.single('image'), memoController.uploadImage);
-router.post('/multipleImages', fileUpload.array('images'), memoController.uploadImages);
 router.post('/linksInfo', memoController.getLinksInfo);
 
 module.exports = router;
