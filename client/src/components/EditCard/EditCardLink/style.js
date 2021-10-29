62b77a543dc762b95eec06060f7be6eee8cad651
Iris Huang <lyaui@outloook.com>
2021-10-29 22:47:57 +0800

    add EditCardLink dark mode

--- client/src/components/EditCard/EditCardLink/style.js
+++ client/src/components/EditCard/EditCardLink/style.js
@@ -3,21 +3,23 @@ import { SLinkItem } from 'components/UI/LinkItem/style.js';
 
 export const SEditCardLink = styled(SLinkItem)`
   height: 56px;
-  border-radius: 8px;
   margin: 16px 0;
+  font-size: 1.4rem;
+  border-radius: 8px;
   cursor: pointer;
   box-shadow: rgb(0 0 0 / 12%) 0px 1px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 1px 0px,
     rgb(0 0 0 / 12%) 0px 1px 4px 0px;
+  background-color: var(--color-link-bg);
 `;
 
 export const SEditCardLinkButton = styled.button`
-  color: #202124;
+  color: var(--color-text);
   opacity: 0.6;
-  font-size: 14px;
+  font-size: 1.4rem;
   font-weight: bold;
   background-color: transparent;
   padding: 8px;
-  margin-bottom: 20px;
+  margin-bottom: 16px;
   transition: all 0.3s;
   &:hover {
     opacity: 1;
