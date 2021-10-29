8634e8327c19e2ad1ba9c0f0aefe3b0e716e4bdb
Iris Huang <lyaui@outloook.com>
2021-10-29 11:19:54 +0800

    add dark mode css variables

--- client/src/GlobalStyle.js
+++ client/src/GlobalStyle.js
@@ -6,37 +6,61 @@ export const GlobalStyle = createGlobalStyle`
 
     /* colors */
     --color-transparent:transparent;
-    --color-white:#ffffff;
-    --color-gray-100:#f1f3f4;
-    --color-gray-200: #e8eaed;
-    --color-gray-700:#5f6368;
-    --color-gray-800:#3c4043;
-    --color-gray-900:#202124;
-  }
+    --color-black: 0, 0%, 0%; // rgb(0%, 0%, 0%)
+    --color-white: 0, 0%, 100%; // hsl(0, 0%, 100%)
 
-  html {
-    box-sizing: border-box;
-    font-size: 62.5%;
-    line-height: 2rem;
+    --color-gray-100: 200, 12%, 95%; // hsl(200, 12%, 95%)
+    --color-gray-200: 216, 12%, 92%; // hsl(216, 12%, 92%)
+                                       
+    --color-gray-500: 210, 6%, 63%; // hsl(210, 6%, 63%)
+    --color-gray-600: 207, 5%, 52%; // hsl(207, 5%, 52%) 
+    --color-gray-700: 213, 5%, 39%; // hsl(213, 5%, 39%)
+    --color-gray-800: 206, 6%, 25%; // 	hsl(206, 6%, 25%)
+    --color-gray-900: 225, 6%, 13%; // hsl(225, 6%, 13%)
+    
   }
 
   *, *:before, *:after {
-    box-sizing: inherit;
+    box-sizing: border-box;
+  }
+
+  html {
+    height: 100%;
+    font-size: 62.5%;
+    font-weight: 500;
+    line-height: 1.5;
+  }
+
+  // refer to root CSS variables
+  body[data-theme='LIGHT'] {
+    --color-text: hsl(var(--color-gray-900));
+    --color-bg: hsl(var(--color-white));
+    --color-link-bg: hsla(var(--color-white),.8);
+    --color-link-url:hsl(var(--color-gray-600));
+    --color-link-border: hsla(var(--color-black),.12)
+  }
+  
+  body[data-theme='DARK'] {
+    --color-text: hsl(var(--color-gray-200));
+    --color-bg: hsl(var(--color-gray-900));
+    --color-link-bg: hsla(var(--color-white),.08);
+    --color-link-url:hsl(var(--color-gray-500));
+    --color-link-border: hsla(var(--color-gray-200),.5)
   }
 
   body{
+    height: 100%;
     font-family: Roboto, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    color: var(--color-text);
+    background-color: var(--color-bg);
     transition: all .3s;
   }
 
-  body[data-theme='LIGHT'] {
-    color: var(--color-gray-900);
-    background-color: var(--color-white);
-}
-  body[data-theme='DARK'] {
-    color: var(--color-gray-200);
-    background-color: var(--color-gray-900);
-}
+  img {
+    max-width: 100%;
+    display: block;
+  } 
 
   div{
     outline:none;
@@ -45,7 +69,6 @@ export const GlobalStyle = createGlobalStyle`
   a{
     text-decoration: none;
   }
-
 
   input{  
     border: none;
