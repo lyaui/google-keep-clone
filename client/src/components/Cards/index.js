a770c67a0ce5dceebfacc62087432c84d4da4ced
Iris Huang <lyaui@outloook.com>
2021-10-31 16:20:55 +0800

    add Cards CSS variables

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import { useUI } from 'contexts/UI-context';
+import { VIEW_MODE } from 'constants/UI.js';
 import Card from 'components/UI/Card';
 import { SCards, SCardsTitle } from 'components/Cards/style.js';
 
@@ -15,8 +16,22 @@ const Cards = ({ memos, title }) => {
 
   return (
     <div ref={masonryRef}>
-      <SCardsTitle isFixedMenu={isFixedMenu}>{title}</SCardsTitle>
-      <SCards className='masonry' viewMode={layout} isFixedMenu={isFixedMenu}>
+      <SCardsTitle
+        style={{
+          '--padding': isFixedMenu ? '0px 80px' : '0px 80px 0px 100px',
+        }}
+      >
+        {title}
+      </SCardsTitle>
+      <SCards
+        className='masonry'
+        style={{
+          '--gap': layout === VIEW_MODE.GRID ? '12px' : '20px',
+          '--columns':
+            layout === VIEW_MODE.GRID ? 'repeat(auto-fill, minmax(230px, 1fr))' : '600px',
+          '--padding': isFixedMenu ? '20px 80px' : '20px 80px 20px 100px',
+        }}
+      >
         {memos.map((card) => (
           <Card key={card._id} card={card} masonryDom={masonryDom} />
         ))}
--- client/src/components/Cards/style.js
+++ client/src/components/Cards/style.js
@@ -1,18 +1,16 @@
 import styled from 'styled-components/macro';
-import { VIEW_MODE } from 'constants/UI.js';
 
 export const SCards = styled.section`
   display: grid;
-  grid-gap: ${(props) => (props.viewMode === VIEW_MODE.GRID ? '12px' : '20px')};
-  grid-template-columns: ${(props) =>
-    props.viewMode === VIEW_MODE.GRID ? 'repeat(auto-fill, minmax(230px, 1fr))' : '600px'};
   justify-content: center;
+  padding: var(--padding);
+  grid-gap: var(--gap);
+  grid-template-columns: var(--columns);
   grid-auto-rows: 0;
-  padding: ${(props) => (props.isFixedMenu ? '20px 80px' : '20px 80px 20px 100px')};
 `;
 
 export const SCardsTitle = styled.div`
   color: var(--color-text);
   margin-top: 60px;
-  padding: ${(props) => (props.isFixedMenu ? '0px 80px' : '0px 80px 0px 100px')};
+  padding: var(--padding);
 `;
