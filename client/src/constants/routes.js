a0bf0277a8fbe5360a2dea7ddee24c531a96cf16
Iris Huang <lyaui@outloook.com>
2021-11-01 21:05:51 +0800

    add search route

--- client/src/constants/routes.js
+++ client/src/constants/routes.js
@@ -5,6 +5,7 @@ export const ROUTE = {
   LABEL: '/label/:labelName',
   MEMO: '/memo/:memoId',
   ARCHIVE: '/archive',
+  SEARCH: '/search',
   BUILD_LABEL_PATH: (labelName) => `/label/${labelName}`,
   BUILD_MEMO_PATH: (memoId) => `/memo/${memoId}`,
 };
