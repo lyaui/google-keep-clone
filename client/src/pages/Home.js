fba3da2046a0ec3167d7f7eb9a175a5f57607461
Iris Huang <lyaui@outloook.com>
2021-11-03 01:17:06 +0800

    refactor Home component

---
+++ client/src/pages/Home.js
@@ -0,0 +1,44 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useRouteMatch } from 'react-router-dom';
+import { getUserMemos } from 'store/memosSlice/memos-action.js';
+import { ROUTE } from 'constants/routes.js';
+import Cards from 'components/Cards';
+import CardEditor from 'components/CardEditor';
+import EditModal from 'components/EditModal';
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const { memos } = useSelector((state) => state.memos);
+
+  const { path, params } = useRouteMatch();
+  const { labelName } = params;
+
+  // fetch memos
+  useEffect(() => {
+    if (path === ROUTE.MEMO) return;
+    const promise = dispatch(getUserMemos());
+    return async () => promise.abort();
+  }, [path, dispatch, labelName]);
+
+  const isArchivePage = path === ROUTE.ARCHIVE;
+  const pinnedMemo = memos.filter((memo) => memo.isPinned && memo.isArchived === isArchivePage);
+  const unpinnedMemo = memos.filter((memo) => !memo.isPinned && memo.isArchived === isArchivePage);
+
+  return (
+    <div>
+      {/* editor */}
+      <CardEditor />
+      {/* isPinned === true */}
+      {pinnedMemo.length > 0 && <Cards memos={pinnedMemo} title={'已固定'} />}
+      {/* isPinned === false */}
+      {unpinnedMemo.length > 0 && (
+        <Cards memos={unpinnedMemo} title={pinnedMemo.length > 0 ? '其他記事' : ''} />
+      )}
+      {/* editModal */}
+      <EditModal />
+    </div>
+  );
+};
+
+export default Home;
