948ca2e30844a6c66bfbc019cbbbdb0bcedd21bb
Iris Huang <lyaui@outloook.com>
2021-11-03 18:04:43 +0800

    add colors filter

--- client/src/components/MemosFilter/index.js
+++ client/src/components/MemosFilter/index.js
@@ -1,28 +1,47 @@
+import { useState } from 'react';
 import {
   SMemosFilter,
   SMemosFilterTitle,
   SMemosFilterUnit,
+  SMemosFilterColor,
   SMemosFilterContainer,
 } from 'components/MemosFilter/style.js';
 
 const MemosFilter = ({ title, type, filter = [] }) => {
+  const defaultUnits = 4;
+  const [showUnitsNum, setShowUnitsNum] = useState(defaultUnits);
+
+  const clickShowMoreHandler = () => setShowUnitsNum(filter.length);
+  const clickShowLessHandler = () => setShowUnitsNum(defaultUnits);
+
   return (
     <SMemosFilter>
       <SMemosFilterTitle>
         <span>{title}</span>
-        {filter.length > 4 && <button>顯示更多</button>}
+        {type === 'colors' ? null : filter.length <= defaultUnits ? null : showUnitsNum ===
+          filter.length ? (
+          <button onClick={clickShowLessHandler}>顯示較少</button>
+        ) : (
+          <button onClick={clickShowMoreHandler}>顯示更多</button>
+        )}
       </SMemosFilterTitle>
       <SMemosFilterContainer
         style={{
           '--color': type === 'type' ? 'var(--color-blue)' : 'var(--color-gray-400)',
         }}
       >
-        {filter.slice(0, 4).map((filter) => (
-          <SMemosFilterUnit type={type}>
-            {filter.icon}
-            {filter.name}
-          </SMemosFilterUnit>
-        ))}
+        {/* types & labels */}
+        {type !== 'colors' &&
+          filter.slice(0, showUnitsNum).map((filter, index) => (
+            <SMemosFilterUnit type={type} key={index}>
+              {filter.icon}
+              <div>{filter.name}</div>
+            </SMemosFilterUnit>
+          ))}
+
+        {/* color */}
+        {type === 'colors' &&
+          filter.map((color) => <SMemosFilterColor style={{ '--color': color.value }} />)}
       </SMemosFilterContainer>
     </SMemosFilter>
   );
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,8 +1,10 @@
 import { useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { useFetchMemos } from 'hooks/fetchMemos-hook.js';
-import { useLocation } from 'react-router-dom';
 import { getUserMemos } from 'store/memosSlice/memos-action.js';
+import { useUI } from 'contexts/UI-context';
+import { PALETTE_COLORS } from 'constants/paletteColors.js';
 import * as Icon from 'components/UI/Icon/index.js';
 import EditModal from 'components/EditModal';
 import Cards from 'components/Cards';
@@ -17,6 +19,9 @@ const Search = () => {
     params,
   });
   const { labels } = useSelector((state) => state.labels);
+  const {
+    UIState: { theme },
+  } = useUI();
 
   const typesFilter = [
     { name: '清單', icon: <Icon.FilterList />, value: 'tasks' },
@@ -30,6 +35,11 @@ const Search = () => {
     value: label.name,
   }));
 
+  const colorsFilter = Object.keys(PALETTE_COLORS).map((color) => ({
+    name: color,
+    value: PALETTE_COLORS[color][theme],
+  }));
+
   return (
     <div>
       {/* search by types */}
@@ -37,7 +47,7 @@ const Search = () => {
       {/* search by labels */}
       <MemosFilter title='標籤' type='labels' filter={labelsFilter} />
       {/* search by colors */}
-      <MemosFilter title='顏色' type='colors' />
+      <MemosFilter title='顏色' type='colors' filter={colorsFilter} />
 
       {/* search results */}
       <Cards memos={memos} />
