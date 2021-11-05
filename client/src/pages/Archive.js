554376a48b9e3e41caa2da030efe57f7baa8f3ea
Iris Huang <lyaui@outloook.com>
2021-11-05 13:54:16 +0800

    add Archive page skeleton

--- client/src/pages/Archive.js
+++ client/src/pages/Archive.js
@@ -1,18 +1,26 @@
 import { useMemo } from 'react';
 import { useFetchMemos } from 'hooks/fetchMemos-hook.js';
 import { getUserMemos } from 'store/memosSlice/memos-action.js';
+import * as Icon from 'components/UI/Icon';
+import SkeletonCards from 'skeletons/SkeletonCards.js';
 import Cards from 'components/Cards';
 import EditModal from 'components/EditModal';
+import Hint from 'components/UI/Hint';
 
 const Archive = () => {
   const params = useMemo(() => ({ isArchived: true }), []);
-  const { pinnedMemo, unpinnedMemo } = useFetchMemos({
+  const { pinnedMemo, unpinnedMemo, isLoading } = useFetchMemos({
     action: getUserMemos,
     params,
   });
 
+  const showHint = pinnedMemo.length === 0 && unpinnedMemo.length === 0;
+
   return (
     <div>
+      {/* skeleton */}
+      {isLoading && <SkeletonCards />}
+
       {/* isPinned === true */}
       {pinnedMemo.length > 0 && <Cards memos={pinnedMemo} title={'已固定'} />}
       {/* isPinned === false */}
@@ -21,6 +29,9 @@ const Archive = () => {
       )}
       {/* editModal */}
       <EditModal />
+
+      {/* hint */}
+      {showHint && <Hint icon={<Icon.Archive />} text='你封存的記事會顯示在這裡' />}
     </div>
   );
 };
