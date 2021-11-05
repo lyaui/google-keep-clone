f2dd3e6af20888baa3d73d4e0565a62f9cfd6310
Iris Huang <lyaui@outloook.com>
2021-11-05 18:31:04 +0800

    add SkeletonEditModal

--- client/src/components/EditModal/index.js
+++ client/src/components/EditModal/index.js
@@ -1,55 +1,27 @@
-import { useState, useEffect } from 'react';
 import { useRouteMatch, useHistory } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { memosActions } from 'store/memosSlice';
+import { useFetchMemos } from 'hooks/fetchMemos-hook.js';
 import { getUserMemoByMemoId } from 'store/memosSlice/memos-action.js';
 import { ROUTE } from 'constants/routes.js';
+import SkeletonEditModal from 'skeletons/SkeletonEditModal.js';
 import Modal from 'components/UI/Modal';
 import EditCard from 'components/EditCard';
 
 const EditModal = () => {
   const history = useHistory();
-  const dispatch = useDispatch();
   const match = useRouteMatch();
-  const { path, url } = match;
-  const [showEditModal, setShowEditModal] = useState(false);
-  const [savedRoute, setSavedRoute] = useState(null);
 
   const { memoId } = match.params;
-  const {
-    memo: { _id: id },
-  } = useSelector((state) => state.memos);
-
-  // fetch memo details
-  useEffect(() => {
-    if (!memoId) return;
-    const promise = dispatch(getUserMemoByMemoId(memoId));
-    return async () => promise.abort();
-  }, [path, dispatch, memoId]);
-
-  // open edit modal
-  useEffect(() => {
-    if (path === ROUTE.MEMO && id) setShowEditModal(true);
-  }, [path, id]);
-
-  // remember path url
-  useEffect(() => {
-    if (path === ROUTE.MEMO) return;
-    setSavedRoute(url);
-  }, [path, url]);
+  const { isLoading } = useFetchMemos({ action: getUserMemoByMemoId, params: memoId });
 
   const closeEditModalHandler = (e) => {
     e.preventDefault();
-    setShowEditModal(false);
-    dispatch(memosActions.resetMemo());
-
-    if (!savedRoute) return history.push(ROUTE.HOME);
-    history.push(savedRoute);
+    history.push(ROUTE.HOME);
   };
 
   return (
-    <Modal showModal={showEditModal} closeModal={closeEditModalHandler}>
-      <EditCard />
+    <Modal showModal={true} closeModal={closeEditModalHandler}>
+      {!isLoading && <EditCard />}
+      {isLoading && <SkeletonEditModal />}
     </Modal>
   );
 };
---
+++ client/src/skeletons/SkeletonEditModal.js
@@ -0,0 +1,52 @@
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import styled from 'styled-components/macro';
+
+const SSkeletonEditModal = styled.div`
+  width: 600px;
+  padding: 12px;
+  background-color: var(--color-bg);
+  border-radius: var(--rounded-lg);
+`;
+
+const SSkeletonEditBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  div {
+    display: flex;
+    > span {
+      padding: 4px;
+    }
+  }
+`;
+
+const SkeletonEditModal = () => {
+  return (
+    <SkeletonTheme
+      baseColor='var(--color-skeleton-bg)'
+      highlightColor='var(--color-skeleton-highlight-bg)'
+    >
+      <SSkeletonEditModal>
+        <Skeleton style={{ marginBottom: '20px' }} height={30} width={200} />
+        <Skeleton style={{ marginBottom: '4px' }} height={16} count={10} />
+
+        <SSkeletonEditBar>
+          <div>
+            {Array.from(Array(5).keys()).map((index) => (
+              <Skeleton key={index} circle height={30} width={30} />
+            ))}
+          </div>
+
+          <Skeleton
+            style={{ display: 'inline-block', marginLeft: 'auto' }}
+            height={28}
+            width={90}
+          />
+        </SSkeletonEditBar>
+      </SSkeletonEditModal>
+    </SkeletonTheme>
+  );
+};
+
+export default SkeletonEditModal;
