fb57d29024a7c79925a67a0a3aee10b58a4cc1a9
Iris Huang <lyaui@outloook.com>
2021-11-06 12:25:29 +0800

    add sample login account

--- client/src/components/LoginPage/LoginImage/index.js
+++ client/src/components/LoginPage/LoginImage/index.js
@@ -1,4 +1,6 @@
 import { useRouteMatch } from 'react-router-dom';
+import { useAuth, login } from 'contexts/auth-context';
+import { useUI, getUserSettings } from 'contexts/UI-context/index.js';
 import { ROUTE } from 'constants/routes.js';
 import loginBackgroundImage from 'assets/images/login-background.jpeg';
 import signupBackgroundImage from 'assets/images/signup-background.jpeg';
@@ -11,6 +13,19 @@ import {
 
 const LoginImage = () => {
   const { path } = useRouteMatch();
+  const { authDispatch } = useAuth();
+  const { UIDispatch } = useUI();
+
+  const loginSampleUserHandler = async () => {
+    const payload = {
+      email: process.env.REACT_APP_SAMPLE_USER,
+      password: process.env.REACT_APP_SAMPLE_PASSWORD,
+    };
+    const loginRes = await login(authDispatch, payload);
+    if (!loginRes.success) return;
+    await getUserSettings(UIDispatch);
+  };
+
   return (
     <SLoginImage
       style={{
@@ -24,7 +39,7 @@ const LoginImage = () => {
           根據顏色或其他屬性，快速過濾及搜尋記事。 <br />
           您可以更快找到所需內容，至於繁瑣的細節，就交給 Keep 為您記住！
         </SLoginImageText>
-        <SLoginImageButton>試用 Google Keep</SLoginImageButton>
+        <SLoginImageButton onClick={loginSampleUserHandler}>試用 Google Keep</SLoginImageButton>
       </div>
     </SLoginImage>
   );
