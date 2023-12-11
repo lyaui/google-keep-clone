9e04fbf1da52be78c6b4df60ed700761691eb806
Iris Huang <lyaui@outloook.com>
2023-12-11 20:54:23 +0800

    feat(constants): UI migrate to TypeScript

--- client/src/constants/UI.ts
+++ client/src/constants/UI.ts
@@ -1,26 +1,17 @@
 export const THEME = {
   LIGHT: 'LIGHT',
   DARK: 'DARK',
-};
+} as const;
 
 export const VIEW_MODE = {
   GRID: 'GRID',
   LIST: 'LIST',
-};
+} as const;
 
 export const SORT = {
   ASCEND: 'ASCEND',
   DESCEND: 'DESCEND',
-};
-
-export const INIT_UI_STATES = {
-  isLoading: false,
-  errorMessage: '',
-  theme: THEME.LIGHT,
-  layout: VIEW_MODE.GRID,
-  sort: SORT.DESCEND,
-  isFixedMenu: false,
-};
+} as const;
 
 export const UI_TYPES = {
   // USER_SETTINGS
@@ -33,4 +24,13 @@ export const UI_TYPES = {
   SETTINGS_UPDATE_REQUEST: 'LOGIN_REQUEST',
   SETTINGS_UPDATE_SUCCESS: 'LOGIN_SUCCESS',
   SETTINGS_UPDATE_FAIL: 'LOGIN_FAIL',
+} as const;
+
+export const INIT_UI_STATES = {
+  isLoading: false,
+  errorMessage: '',
+  theme: THEME.LIGHT,
+  layout: VIEW_MODE.GRID,
+  sort: SORT.DESCEND,
+  isFixedMenu: false,
 };
