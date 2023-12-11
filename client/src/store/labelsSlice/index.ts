41cd7922b4400dd6763540aaa86d0e2356326045
Iris Huang <lyaui@outloook.com>
2023-12-12 04:19:29 +0800

    feat(store): labelsSlice migrate to TypeScript

--- client/src/store/labelsSlice/index.ts
+++ client/src/store/labelsSlice/index.ts
@@ -1,76 +1,88 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+
 import {
   getUserLabels,
   addLabel,
   updateLabel,
   deleteLabel,
-} from '@/store/labelsSlice/labels-action.js';
+} from '@/store/labelsSlice/labels-action';
+import { MemoLabel } from '@/types';
+
+interface LabelsState {
+  isLoading: boolean;
+  errorMessage: string;
+  labels: MemoLabel[];
+}
 
 const INIT_LABELS_STATE = {
   isLoading: false,
   errorMessage: '',
   labels: [],
-};
+} as LabelsState;
 
 const labelsSlice = createSlice({
   name: 'labels',
   initialState: INIT_LABELS_STATE,
+  reducers: {},
   extraReducers: {
     // getUserLabels
     [getUserLabels.pending](state) {
       state.isLoading = true;
     },
-    [getUserLabels.fulfilled](state, { payload: labels }) {
+    [getUserLabels.fulfilled](state, action: PayloadAction<MemoLabel[]>) {
       state.isLoading = false;
       state.errorMessage = '';
-      state.labels = labels;
+      state.labels = action.payload;
     },
-    [getUserLabels.rejected](state, { payload: errorMessage }) {
+    [getUserLabels.rejected](state, action: PayloadAction<string>) {
       state.isLoading = false;
-      state.errorMessage = errorMessage;
+      state.errorMessage = action.payload;
     },
 
     // addLabel
     [addLabel.pending](state) {
       state.isLoading = true;
     },
-    [addLabel.fulfilled](state, { payload: label }) {
+    [addLabel.fulfilled](state, action: PayloadAction<MemoLabel>) {
       state.isLoading = false;
       state.errorMessage = '';
-      state.labels = [...state.labels, label];
+      state.labels = [...state.labels, action.payload];
     },
-    [addLabel.rejected](state, { payload: errorMessage }) {
+    [addLabel.rejected](state, action: PayloadAction<string>) {
       state.isLoading = false;
-      state.errorMessage = errorMessage;
+      state.errorMessage = action.payload;
     },
 
     // updateLabel
     [updateLabel.pending](state) {
       state.isLoading = true;
     },
-    [updateLabel.fulfilled](state, { payload: label }) {
+    [updateLabel.fulfilled](state, action: PayloadAction<MemoLabel>) {
+      const label = action.payload;
       state.isLoading = false;
       state.errorMessage = '';
       const index = state.labels.findIndex((item) => item._id === label._id);
       state.labels[index] = label;
     },
-    [updateLabel.rejected](state, { payload: errorMessage }) {
+    [updateLabel.rejected](state, action: PayloadAction<string>) {
       state.isLoading = false;
-      state.errorMessage = errorMessage;
+      state.errorMessage = action.payload;
     },
 
     // deleteLabel
     [deleteLabel.pending](state) {
       state.isLoading = true;
     },
-    [deleteLabel.fulfilled](state, { payload: labelId }) {
+    [deleteLabel.fulfilled](state, action: PayloadAction<string>) {
       state.isLoading = false;
       state.errorMessage = '';
+
+      const labelId = action.payload;
       state.labels = state.labels.filter((label) => label._id !== labelId);
     },
-    [deleteLabel.rejected](state, { payload: errorMessage }) {
+    [deleteLabel.rejected](state, action: PayloadAction<string>) {
       state.isLoading = false;
-      state.errorMessage = errorMessage;
+      state.errorMessage = action.payload;
     },
   },
 });
