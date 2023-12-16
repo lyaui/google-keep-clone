8a93b7d05e1de0b920e789620026df694a8741f1
Iris Huang <lyaui@outloook.com>
2023-12-17 00:30:36 +0800

    feat(Home): migrate to ts

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,55 @@
+import { useMemo } from 'react';
+import { useFetchMemos } from '@/hooks/fetchMemos-hook';
+import { getUserMemos } from '@/store/memosSlice/memos-action';
+import * as Icon from '@/components/UI/Icon';
+import SkeletonEditor from '@/skeletons/SkeletonEditor';
+import SkeletonCards from '@/skeletons/SkeletonCards';
+import MemoCards from '@/components/MemoCards';
+import CardEditor from '@/components/CardEditor';
+import Hint from '@/components/UI/Hint';
+
+const Home = () => {
+  const params = useMemo(() => ({ isArchived: false }), []);
+  const { pinnedMemo, unpinnedMemo, isLoading } = useFetchMemos({
+    action: getUserMemos,
+    params,
+  });
+
+  const showHint =
+    pinnedMemo.length === 0 && unpinnedMemo.length === 0 && !isLoading;
+
+  return (
+    <div>
+      {/* skeleton */}
+      {isLoading ? (
+        <>
+          <SkeletonEditor />
+          <SkeletonCards />
+        </>
+      ) : (
+        <>
+          {/* editor */}
+          <CardEditor />
+          {/* isPinned === true */}
+          {pinnedMemo.length > 0 && (
+            <MemoCards memos={pinnedMemo} title={'已固定'} />
+          )}
+          {/* isPinned === false */}
+          {unpinnedMemo.length > 0 && (
+            <MemoCards
+              memos={unpinnedMemo}
+              title={pinnedMemo.length > 0 ? '其他記事' : ''}
+            />
+          )}
+        </>
+      )}
+
+      {/* hint */}
+      {showHint && (
+        <Hint icon={<Icon.Bulb />} text="你新增的記事會顯示在這裡" />
+      )}
+    </div>
+  );
+};
+
+export default Home;
