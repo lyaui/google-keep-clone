8aa37f0bcd2aac1c8dcf015344356183cf0a717b
Iris Huang <lyaui@outloook.com>
2023-12-17 06:42:40 +0800

    feat(Label): migrate to ts

---
+++ client/src/pages/Label.tsx
@@ -0,0 +1,37 @@
+import { useMemo } from 'react';
+import { useFetchMemos } from '@/hooks/fetchMemos-hook';
+import { useParams } from 'react-router-dom';
+
+import { getUserMemosByLabelName } from '@/store/memosSlice/memos-action';
+import * as Icon from '@/components/UI/Icon';
+import MemosPage from '@/components/MemosPage';
+
+function Label() {
+  const { labelName } = useParams();
+
+  const params = useMemo(
+    () => ({ labelName, query: { isArchived: false } }),
+    [labelName]
+  );
+
+  const { pinnedMemo, unpinnedMemo, isLoading } = useFetchMemos({
+    action: getUserMemosByLabelName,
+    params,
+  });
+
+  const hintConfig = {
+    text: '目前還沒有記事加上這個標籤',
+    icon: <Icon.LabelOutline />,
+  };
+
+  return (
+    <MemosPage
+      isLoading={isLoading}
+      pinnedMemo={pinnedMemo}
+      unpinnedMemo={unpinnedMemo}
+      hintConfig={hintConfig}
+    />
+  );
+}
+
+export default Label;
