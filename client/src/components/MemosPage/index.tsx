7e45f383ea50bee04f2a18ded43542660e928b8c
Iris Huang <lyaui@outloook.com>
2023-12-21 00:27:44 +0800

    feat(MemosPage): scroll to top when pathname changes

--- client/src/components/MemosPage/index.tsx
+++ client/src/components/MemosPage/index.tsx
@@ -1,4 +1,5 @@
-import type { ReactNode } from 'react';
+import { useEffect, useRef, type ReactNode } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import type { Memo } from '@/types';
 import SkeletonEditor from '@/skeletons/SkeletonEditor';
@@ -23,6 +24,9 @@ function MemosPage({
   unpinnedMemo = [],
   hintConfig,
 }: MemosPageProps) {
+  const { pathname } = useLocation();
+  const ref = useRef<HTMLDivElement>(null);
+
   const showHint = pinnedMemo.length === 0 && unpinnedMemo.length === 0;
   const sections = [
     { title: '已固定', memos: pinnedMemo },
@@ -32,8 +36,13 @@ function MemosPage({
     },
   ].filter((_section) => _section.memos.length > 0);
 
+  useEffect(() => {
+    if (!ref.current) return;
+    ref.current.scrollTo({ top: 0 });
+  }, [pathname]);
+
   return (
-    <div>
+    <main ref={ref} style={{ overflow: 'auto' }}>
       {isLoading ? (
         <>
           <SkeletonEditor />
@@ -45,10 +54,10 @@ function MemosPage({
           {sections.map((_section) => (
             <MemoCards key={_section.title} {..._section} />
           ))}
+          {showHint && <Hint {...hintConfig} />}
         </>
       )}
-      {showHint && <Hint {...hintConfig} />}
-    </div>
+    </main>
   );
 }
 
