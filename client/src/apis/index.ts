ba2d9331b38c33134803a50c6c3bf28887cbec57
Iris Huang <lyaui@outloook.com>
2023-12-23 22:49:05 +0800

    fix(apis): axiosBaseQuery

--- client/src/apis/index.ts
+++ client/src/apis/index.ts
@@ -1,34 +1,58 @@
+import type { BaseQueryFn } from '@reduxjs/toolkit/query';
 import axios from 'axios';
+import type { AxiosRequestConfig, AxiosError } from 'axios';
 
-interface AxiosRequest {
-  baseUrl: string;
-  contentType?: string;
-}
+const instance = axios.create({
+  baseURL: `${import.meta.env.REACT_APP_SERVER_BASE_URL}/api`,
+});
 
-function axiosRequest({ baseUrl, contentType, ...others }: AxiosRequest) {
-  const instance = axios.create({
-    baseURL: `${import.meta.env.REACT_APP_SERVER_BASE_URL}/api${baseUrl}`,
-    headers: {
-      'Content-Type': contentType || 'application/json',
-    },
-    ...others,
-  });
-
-  instance.interceptors.request.use(
-    (config) => {
-      const userInfoStr = localStorage.getItem('userInfo');
-      const { token } = userInfoStr ? JSON.parse(userInfoStr) : '';
-      if (config.headers) {
-        config.headers.Authorization = `JWT ${token}`;
-      }
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
+instance.interceptors.request.use(
+  (config) => {
+    const userInfoStr = localStorage.getItem('userInfo');
+    const { token } = userInfoStr ? JSON.parse(userInfoStr) : '';
+    if (config.headers) {
+      config.headers.Authorization = `JWT ${token}`;
     }
-  );
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-  return instance;
-}
+const axiosBaseQuery =
+  (
+    { baseUrl }: { baseUrl: string } = { baseUrl: '' }
+  ): BaseQueryFn<
+    {
+      url: string;
+      method?: AxiosRequestConfig['method'];
+      data?: AxiosRequestConfig['data'];
+      params?: AxiosRequestConfig['params'];
+      headers?: AxiosRequestConfig['headers'];
+    },
+    unknown,
+    unknown
+  > =>
+  async ({ url, method, data, params, headers }) => {
+    try {
+      const result = await instance({
+        url: baseUrl + url,
+        method,
+        data,
+        params,
+        headers,
+      });
+      return { data: result.data };
+    } catch (axiosError) {
+      const err = axiosError as AxiosError;
+      return {
+        error: {
+          status: err.response?.status,
+          data: err.response?.data || err.message,
+        },
+      };
+    }
+  };
 
-export default axiosRequest;
+export default axiosBaseQuery;
