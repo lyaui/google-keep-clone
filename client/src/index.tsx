d934538b1ee1923f89681cfc822676db6e33713c
Iris Huang <lyaui@outloook.com>
2024-01-17 03:03:54 +0800

    fix(index): import createRoot from react-dom/client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import 'normalize.css';
 
@@ -9,7 +9,7 @@ import { UIProvider } from '@/contexts/UI-context';
 import { GlobalStyle } from '@/GlobalStyle';
 import App from '@/App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <AuthProvider>
