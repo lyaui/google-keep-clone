a65dbf28003930eb44ea02a9a60c2dc503b0d02c
Iris Huang <lyaui@outlook.com>
2024-03-09 22:30:52 +0800

    chore(install): react query

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -7,11 +7,12 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@tanstack/query'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
